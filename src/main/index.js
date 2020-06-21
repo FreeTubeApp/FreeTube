93688dc2b4b01907f1dabccf290c138b1810d15f
Airradda <3744856+Airradda@users.noreply.github.com>
2020-06-21 04:16:03 -0500

    Enable "autoHideMenuBar" on electron window

--- src/main/index.js
+++ src/main/index.js
@@ -63,6 +63,7 @@ function createWindow () {
     backgroundColor: '#fff',
     width: 960,
     height: 540,
+    autoHideMenuBar: true,
     // useContentSize: true,
     webPreferences: {
       nodeIntegration: true,
