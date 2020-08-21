6f18da09861256d5b0690aafb0ecc2aebb69112e
Preston <freetubeapp@protonmail.com>
2020-08-21 10:16:25 -0400

    Remove icon from build

--- src/main/index.js
+++ src/main/index.js
@@ -64,7 +64,6 @@ function createWindow () {
     backgroundColor: '#fff',
     width: 960,
     height: 540,
-    icon: require('path').join(__dirname, '../../_icons/iconColor.png'),
     autoHideMenuBar: true,
     // useContentSize: true,
     webPreferences: {
