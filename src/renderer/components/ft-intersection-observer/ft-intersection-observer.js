7cbcfae722fe2fcf24b691a974bb24d53e35dcc3
Violet Rose <VioletRose@users.noreply.github.com>
2020-10-03 12:32:09 -0700

    Created ft-intersection-observer component.

---
+++ src/renderer/components/ft-intersection-observer/ft-intersection-observer.js
@@ -0,0 +1,50 @@
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'FtIntersectionObserver',
+  props: {
+    checkOnMount: {
+      type: Boolean,
+      default: false
+    },
+    margin: {
+      type: String,
+      default: '0px 0px 0px 0px'
+    },
+    observeParent: {
+      type: Boolean,
+      default: false
+    },
+    threshold: {
+      type: Number,
+      default: 1
+    }
+  },
+  data() {
+    const observer = new IntersectionObserver(this.handleIntersection, {
+      rootMargin: this.margin,
+      threshold: this.threshold
+    })
+    const runOnce = false
+
+    return {
+      observer,
+      runOnce
+    }
+  },
+  mounted() {
+    this.observer.observe(this.observeParent ? this.$refs.elem.parentElement : this.$refs.elem)
+  },
+  beforeDestroy() {
+    this.observer.disconnect()
+  },
+  methods: {
+    handleIntersection(entries) {
+      if (this.runOnce || this.checkOnMount) {
+        this.$emit(entries[0].isIntersecting ? 'intersected' : 'unintersected')
+      } else {
+        this.runOnce = true
+      }
+    }
+  }
+})
---
+++ src/renderer/components/ft-intersection-observer/ft-intersection-observer.vue
@@ -0,0 +1,5 @@
+<template>
+  <div ref="elem" />
+</template>
+
+<script src="./ft-intersection-observer.js" />
