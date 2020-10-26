c158793f282a8818d5d3823b6072d79bf2a10279
Laptop Duane <duanejaspers@gmail.com>
2020-10-26 12:34:55 +0100

    Remove isLiveContent, so only now live is true

--- src/renderer/views/Watch/Watch.js
+++ src/renderer/views/Watch/Watch.js
@@ -234,7 +234,7 @@ export default Vue.extend({
             this.videoLikeCount = result.videoDetails.likes
             this.videoDislikeCount = result.videoDetails.dislikes
           }
-          this.isLive = result.player_response.videoDetails.isLiveContent || result.player_response.videoDetails.isLive
+          this.isLive = result.player_response.videoDetails.isLive
           this.isUpcoming = result.player_response.videoDetails.isUpcoming ? result.player_response.videoDetails.isUpcoming : false
 
           if (!this.isLive && !this.isUpcoming) {
