3c8dfb84b555cb891a99758afd95a4bf28d0ba31
Duane Jaspers <37299563+DuaneJaspers@users.noreply.github.com>
2020-10-31 15:57:51 +0100

    Change to only consider currently live broadcasts as livestreams. (#728)
    * Remove isLiveContent, so only now live is true
    
    * Change Published on to Streamed on when applicable

--- src/renderer/views/Watch/Watch.js
+++ src/renderer/views/Watch/Watch.js
@@ -44,6 +44,7 @@ export default Vue.extend({
       showYouTubeNoCookieEmbed: false,
       hidePlayer: false,
       isLive: false,
+      isLiveContent: false,
       isUpcoming: false,
       upcomingTimestamp: null,
       activeFormat: 'legacy',
@@ -234,7 +235,8 @@ export default Vue.extend({
             this.videoLikeCount = result.videoDetails.likes
             this.videoDislikeCount = result.videoDetails.dislikes
           }
-          this.isLive = result.player_response.videoDetails.isLiveContent || result.player_response.videoDetails.isLive
+          this.isLive = result.player_response.videoDetails.isLive
+          this.isLiveContent = result.player_response.videoDetails.isLiveContent
           this.isUpcoming = result.player_response.videoDetails.isUpcoming ? result.player_response.videoDetails.isUpcoming : false
 
           if (!this.isLive && !this.isUpcoming) {
