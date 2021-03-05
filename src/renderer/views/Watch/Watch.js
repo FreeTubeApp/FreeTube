012335dc62a95b3110ae99ecbc0784dd12edf51c
Svallinn <41585298+Svallinn@users.noreply.github.com>
2021-03-05 19:49:40 +0000

    Verify paused player before autoplaying next video
    This prevents the autoplay from playing the next video if the current video gets manually replayed by the user

--- src/renderer/views/Watch/Watch.js
+++ src/renderer/views/Watch/Watch.js
@@ -810,7 +810,10 @@ export default Vue.extend({
     handleVideoEnded: function () {
       if (this.watchingPlaylist) {
         this.playNextTimeout = setTimeout(() => {
-          this.$refs.watchVideoPlaylist.playNextVideo()
+          const player = this.$refs.videoPlayer.player
+          if (player !== null && player.paused()) {
+            this.$refs.watchVideoPlaylist.playNextVideo()
+          }
         }, 5000)
 
         this.showToast({
@@ -825,15 +828,18 @@ export default Vue.extend({
         })
       } else if (this.playNextVideo) {
         this.playNextTimeout = setTimeout(() => {
-          const nextVideoId = this.recommendedVideos[0].videoId
-          this.$router.push(
-            {
-              path: `/watch/${nextVideoId}`
-            }
-          )
-          this.showToast({
-            message: this.$t('Playing Next Video')
-          })
+          const player = this.$refs.videoPlayer.player
+          if (player !== null && player.paused()) {
+            const nextVideoId = this.recommendedVideos[0].videoId
+            this.$router.push(
+              {
+                path: `/watch/${nextVideoId}`
+              }
+            )
+            this.showToast({
+              message: this.$t('Playing Next Video')
+            })
+          }
         }, 5000)
 
         this.showToast({
