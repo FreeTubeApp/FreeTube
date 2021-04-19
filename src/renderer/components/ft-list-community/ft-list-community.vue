ebfe1c8ac2d528a08718bb3a240d4afa1fce6135
Luca <hohmann.luca@tutanota.com>
2021-04-19 22:56:13 +0200

    Images are now displayed in the community tab

--- src/renderer/components/ft-list-community/ft-list-community.vue
+++ src/renderer/components/ft-list-community/ft-list-community.vue
@@ -1,9 +1,21 @@
 <template>
-  <div>
-    "HELLO"
+  <div
+    class="ft-list-community ft-list-item"
+    :class="{
+      list: (listType === 'list' || forceListType === 'list') && forceListType !== 'grid',
+      grid: (listType === 'grid' || forceListType === 'list') && forceListType !== 'list',
+      [appearance]: true,
+      watched: addWatchedStyle
+    }"
+  >
+    <div
+    >
+      <img
+        :src="image"
+      >
+    </div>
   </div>
 </template>
 
 <script src="./ft-list-community.js" />
 <style scoped src="./ft-list-community.sass" lang="sass" />
-
