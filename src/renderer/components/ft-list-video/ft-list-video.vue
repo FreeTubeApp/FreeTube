c5be0796f2262e63972cdee2b8ee38d2cc70b721
ChunkyProgrammer <78101139+ChunkyProgrammer@users.noreply.github.com>
2021-06-25 19:09:50 -0400

    fix hide live streams

--- src/renderer/components/ft-list-video/ft-list-video.vue
+++ src/renderer/components/ft-list-video/ft-list-video.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    v-if="(!isLive && duration !== '0:00') || ((isLive || duration === null) && !hideLiveStreams)"
+    v-if="(!isLive && duration !== '0:00') || (!hideLiveStreams)"
     class="ft-list-video ft-list-item"
     :class="{
       list: (listType === 'list' || forceListType === 'list') && forceListType !== 'grid',
