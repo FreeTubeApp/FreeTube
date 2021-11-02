9bf4742cf9dbe8d0dd29f87389c86afdc0d2097c
ChunkyProgrammer <78101139+ChunkyProgrammer@users.noreply.github.com>
2021-11-02 07:45:50 -0400

    Recognize more channel links (#1455)
    * Update channelPattern regex
    
    * redirect to relevant sub page
    
    * Simplify regex
    
    Co-authored-by: PikachuEXE <pikachuexe@gmail.com>
    
    * fix regression
    
    - fix regression from commit 76f0d7512a11d57598bda6fd0142767aec6218af
    - add comment to explain regex
    
    Co-authored-by: Preston <freetubeapp@protonmail.com>
    Co-authored-by: PikachuEXE <pikachuexe@gmail.com>

--- src/renderer/App.js
+++ src/renderer/App.js
@@ -398,10 +398,10 @@ export default Vue.extend({
           }
 
           case 'channel': {
-            const { channelId } = result
+            const { channelId, subPath } = result
 
             this.$router.push({
-              path: `/channel/${channelId}`
+              path: `/channel/${channelId}/${subPath}`
             })
             break
           }
