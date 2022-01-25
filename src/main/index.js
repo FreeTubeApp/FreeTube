589bbc4d827bd7860fa8bbcffda34e6d16c73f02
constraintautomaton <constraintautomaton@protonmail.com>
2022-01-25 18:11:48 -0500

    simplify context menu entry condition

--- src/main/index.js
+++ src/main/index.js
@@ -30,7 +30,7 @@ function runApp() {
       },
       {
         label: 'Open in a New Window',
-        visible: parameters.linkURL.includes('localhost') || parameters.mediaType === 'image',
+        visible: parameters.linkURL.includes('localhost'),
         click: () => {
           createWindow(false, parameters.linkURL)
         }
