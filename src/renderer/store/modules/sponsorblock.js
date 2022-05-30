287e51aa5521c1b51246c931e0ebaf89c4794b05
absidue <48293849+absidue@users.noreply.github.com>
2022-05-30 19:33:47 +0200

    Use the builtin subtle crypto library instead of node-forge

--- src/renderer/store/modules/sponsorblock.js
+++ src/renderer/store/modules/sponsorblock.js
@@ -1,5 +1,4 @@
 import $ from 'jquery'
-import forge from 'node-forge'
 
 const state = {}
 const getters = {}
@@ -7,22 +6,30 @@ const getters = {}
 const actions = {
   sponsorBlockSkipSegments ({ rootState }, { videoId, categories }) {
     return new Promise((resolve, reject) => {
-      const messageDigestSha256 = forge.md.sha256.create()
-      messageDigestSha256.update(videoId)
-      const videoIdHashPrefix = messageDigestSha256.digest().toHex().substring(0, 4)
-      const requestUrl = `${rootState.settings.sponsorBlockUrl}/api/skipSegments/${videoIdHashPrefix}?categories=${JSON.stringify(categories)}`
+      const videoIdBuffer = new TextEncoder().encode(videoId)
 
-      $.getJSON(requestUrl, (response) => {
-        const segments = response
-          .filter((result) => result.videoID === videoId)
-          .flatMap((result) => result.segments)
-        resolve(segments)
-      }).fail((xhr, textStatus, error) => {
-        console.log(xhr)
-        console.log(textStatus)
-        console.log(requestUrl)
-        console.log(error)
-        reject(xhr)
+      crypto.subtle.digest('SHA-256', videoIdBuffer).then((hashBuffer) => {
+        const hashArray = Array.from(new Uint8Array(hashBuffer))
+
+        const videoIdHashPrefix = hashArray
+          .map(byte => byte.toString(16).padStart(2, '0'))
+          .slice(0, 4)
+          .join('')
+
+        const requestUrl = `${rootState.settings.sponsorBlockUrl}/api/skipSegments/${videoIdHashPrefix}?categories=${JSON.stringify(categories)}`
+
+        $.getJSON(requestUrl, (response) => {
+          const segments = response
+            .filter((result) => result.videoID === videoId)
+            .flatMap((result) => result.segments)
+          resolve(segments)
+        }).fail((xhr, textStatus, error) => {
+          console.log(xhr)
+          console.log(textStatus)
+          console.log(requestUrl)
+          console.log(error)
+          reject(xhr)
+        })
       })
     })
   }
