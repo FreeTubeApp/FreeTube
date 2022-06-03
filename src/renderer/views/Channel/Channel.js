74dc309803c061cc5f55d8bae3f6812384d78fed
ChunkyProgrammer <78101139+ChunkyProgrammer@users.noreply.github.com>
2022-06-03 08:52:35 -0400

    Fix: Update thumbnail url and channel name for subscriptions when change detected (#1783)
    * Update thumbnails of subscriptions
    
    * refactor
    
    * update channel names
    
    * movie update thumbnail action to profiles.js
    
    * fix updating with invidious
    
    * apply code changes
    
    * simplify update condition logic
    
    * remove unneeded variables
    
    * inline variables, use for of
    
    * fix channel null check

--- src/renderer/views/Channel/Channel.js
+++ src/renderer/views/Channel/Channel.js
@@ -255,16 +255,19 @@ export default Vue.extend({
           return
         }
 
-        this.id = response.authorId
-        this.channelName = response.author
+        const channelId = response.authorId
+        const channelName = response.author
+        const channelThumbnailUrl = response.authorThumbnails[2].url
+        this.id = channelId
+        this.channelName = channelName
         document.title = `${this.channelName} - ${process.env.PRODUCT_NAME}`
         if (this.hideChannelSubscriptions || response.subscriberCount === 0) {
           this.subCount = null
         } else {
           this.subCount = response.subscriberCount.toFixed(0)
         }
-        console.log(response)
-        this.thumbnailUrl = response.authorThumbnails[2].url
+        this.thumbnailUrl = channelThumbnailUrl
+        this.updateSubscriptionDetails({ channelThumbnailUrl, channelName, channelId })
         this.channelDescription = autolinker.link(response.description)
         this.relatedChannels = response.relatedChannels.items
         this.relatedChannels.forEach(relatedChannel => {
@@ -371,15 +374,19 @@ export default Vue.extend({
         }
 
         console.log(response)
-        this.channelName = response.author
+        const channelName = response.author
+        const channelId = response.authorId
+        this.channelName = channelName
         document.title = `${this.channelName} - ${process.env.PRODUCT_NAME}`
-        this.id = response.authorId
+        this.id = channelId
         if (this.hideChannelSubscriptions) {
           this.subCount = null
         } else {
           this.subCount = response.subCount
         }
-        this.thumbnailUrl = response.authorThumbnails[3].url.replace('https://yt3.ggpht.com', `${this.currentInvidiousInstance}/ggpht/`)
+        const thumbnail = response.authorThumbnails[3].url
+        this.thumbnailUrl = thumbnail.replace('https://yt3.ggpht.com', `${this.currentInvidiousInstance}/ggpht/`)
+        this.updateSubscriptionDetails({ channelThumbnailUrl: thumbnail, channelName: channelName, channelId: channelId })
         this.channelDescription = autolinker.link(response.description)
         this.relatedChannels = response.relatedChannels.map((channel) => {
           channel.authorThumbnails[channel.authorThumbnails.length - 1].url = channel.authorThumbnails[channel.authorThumbnails.length - 1].url.replace('https://yt3.ggpht.com', `${this.currentInvidiousInstance}/ggpht/`)
@@ -779,7 +786,8 @@ export default Vue.extend({
       'showToast',
       'updateProfile',
       'invidiousGetChannelInfo',
-      'invidiousAPICall'
+      'invidiousAPICall',
+      'updateSubscriptionDetails'
     ])
   }
 })
