0539e681645960ae272c715a0546a2d39376f8b7
absidue <48293849+absidue@users.noreply.github.com>
2022-07-28 04:10:22 +0200

    Fix blank space in the subscription settings (#2372)
    * Fix blank space in the subscription settings
    
    * Remove the unused code instead of hiding it

--- src/renderer/components/subscription-settings/subscription-settings.js
+++ src/renderer/components/subscription-settings/subscription-settings.js
@@ -1,31 +1,17 @@
 import Vue from 'vue'
 import { mapActions } from 'vuex'
-import FtCard from '../ft-card/ft-card.vue'
 import FtToggleSwitch from '../ft-toggle-switch/ft-toggle-switch.vue'
-import FtButton from '../ft-button/ft-button.vue'
-import FtSelect from '../ft-select/ft-select.vue'
 import FtFlexBox from '../ft-flex-box/ft-flex-box.vue'
 
 export default Vue.extend({
   name: 'SubscriptionSettings',
   components: {
-    'ft-card': FtCard,
     'ft-toggle-switch': FtToggleSwitch,
-    'ft-button': FtButton,
-    'ft-select': FtSelect,
     'ft-flex-box': FtFlexBox
   },
   data: function () {
     return {
-      title: 'Subscription Settings',
-      viewNames: [
-        'Basic',
-        'Modern'
-      ],
-      viewValues: [
-        'basic',
-        'modern'
-      ]
+      title: 'Subscription Settings'
     }
   },
   computed: {
