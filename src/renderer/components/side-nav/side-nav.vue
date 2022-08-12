180a89d7c693882d1e9df222a9cfc520a14e519d
PrestonN <freetubeapp@protonmail.com>
2022-08-11 21:46:41 -0400

    Move channels page to more menu in mobile UI

--- src/renderer/components/side-nav/side-nav.vue
+++ src/renderer/components/side-nav/side-nav.vue
@@ -33,7 +33,7 @@
         </p>
       </div>
       <div
-        class="navOption mobileShow"
+        class="navOption mobileHidden"
         role="button"
         tabindex="0"
         :title="$t('Channels.Channels')"
