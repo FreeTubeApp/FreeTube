23703e96ab6880548fe3e982faaffcb3ad7fc0f2
Aiz <dev@aiz.moe>
2022-09-29 23:21:24 +0200

    reduce distance between switches by adding compact=true

--- src/renderer/components/subscription-settings/subscription-settings.vue
+++ src/renderer/components/subscription-settings/subscription-settings.vue
@@ -8,12 +8,14 @@
           :label="$t('Settings.Subscription Settings.Fetch Automatically')"
           :default-value="fetchSubscriptionsAutomatically"
           :tooltip="$t('Tooltips.Subscription Settings.Fetch Automatically')"
+          :compact="true"
           @change="updateFetchSubscriptionsAutomatically"
         />
         <ft-toggle-switch
           :label="$t('Settings.Subscription Settings.Fetch Feeds from RSS')"
           :default-value="useRssFeeds"
           :tooltip="$t('Tooltips.Subscription Settings.Fetch Feeds from RSS')"
+          :compact="true"
           @change="updateUseRssFeeds"
         />
       </div>
@@ -21,6 +23,7 @@
         <ft-toggle-switch
           :label="$t('Settings.Subscription Settings.Hide Videos on Watch')"
           :default-value="hideWatchedSubs"
+          :compact="true"
           @change="updateHideWatchedSubs"
         />
       </div>
