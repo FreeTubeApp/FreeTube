d7390878bf82bbdc3b5e80aff06909ac34021d96
Aiz <dev@aiz.moe>
2022-09-29 18:50:35 +0200

    Use a switch column grid for the subscription settings
    This matches the layout other settings components

--- src/renderer/components/subscription-settings/subscription-settings.vue
+++ src/renderer/components/subscription-settings/subscription-settings.vue
@@ -2,25 +2,29 @@
   <ft-settings-section
     :title="$t('Settings.Subscription Settings.Subscription Settings')"
   >
-    <ft-flex-box class="settingsFlexStart500px">
-      <ft-toggle-switch
-        :label="$t('Settings.Subscription Settings.Hide Videos on Watch')"
-        :default-value="hideWatchedSubs"
-        @change="updateHideWatchedSubs"
-      />
-      <ft-toggle-switch
-        :label="$t('Settings.Subscription Settings.Fetch Feeds from RSS')"
-        :default-value="useRssFeeds"
-        :tooltip="$t('Tooltips.Subscription Settings.Fetch Feeds from RSS')"
-        @change="updateUseRssFeeds"
-      />
-      <ft-toggle-switch
-        :label="$t('Settings.Subscription Settings.Fetch Automatically')"
-        :default-value="fetchSubscriptionsAutomatically"
-        :tooltip="$t('Tooltips.Subscription Settings.Fetch Automatically')"
-        @change="updateFetchSubscriptionsAutomatically"
-      />
-    </ft-flex-box>
+    <div class="switchColumnGrid">
+      <div class="switchColumn">
+        <ft-toggle-switch
+          :label="$t('Settings.Subscription Settings.Fetch Automatically')"
+          :default-value="fetchSubscriptionsAutomatically"
+          :tooltip="$t('Tooltips.Subscription Settings.Fetch Automatically')"
+          @change="updateFetchSubscriptionsAutomatically"
+        />
+        <ft-toggle-switch
+          :label="$t('Settings.Subscription Settings.Fetch Feeds from RSS')"
+          :default-value="useRssFeeds"
+          :tooltip="$t('Tooltips.Subscription Settings.Fetch Feeds from RSS')"
+          @change="updateUseRssFeeds"
+        />
+      </div>
+      <div class="switchColumn">
+        <ft-toggle-switch
+          :label="$t('Settings.Subscription Settings.Hide Videos on Watch')"
+          :default-value="hideWatchedSubs"
+          @change="updateHideWatchedSubs"
+        />
+      </div>
+    </div>
   </ft-settings-section>
 </template>
 
