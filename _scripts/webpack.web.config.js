b2b7173379ae7a33a121774523fddd6c9ff7b09d
absidue <48293849+absidue@users.noreply.github.com>
2022-09-30 17:57:09 +0200

    Fix the development only js-yaml dependency being included in the release builds

--- _scripts/webpack.web.config.js
+++ _scripts/webpack.web.config.js
@@ -189,6 +189,12 @@ if (isDevMode) {
             },
           },
       ]
+    }),
+    // webpack doesn't get rid of js-yaml even though it isn't used in the production builds
+    // so we need to manually tell it to ignore any imports for `js-yaml`
+    new webpack.IgnorePlugin({
+      resourceRegExp: /^js-yaml$/,
+      contextRegExp: /i18n$/
     })
   )
 }
