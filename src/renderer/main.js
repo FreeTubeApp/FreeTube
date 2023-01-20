c3c63654c75c0fb109e60a966c263f20d43aa914
absidue <48293849+absidue@users.noreply.github.com>
2023-01-20 12:00:07 +0100

    Use a single import for the brand icons (#3094)

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -55,10 +55,12 @@ import {
   faTimesCircle,
   faUsers
 } from '@fortawesome/free-solid-svg-icons'
-import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub'
-import { faBitcoin } from '@fortawesome/free-brands-svg-icons/faBitcoin'
-import { faMonero } from '@fortawesome/free-brands-svg-icons/faMonero'
-import { faMastodon } from '@fortawesome/free-brands-svg-icons/faMastodon'
+import {
+  faBitcoin,
+  faGithub,
+  faMastodon,
+  faMonero
+} from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 Vue.config.devtools = process.env.NODE_ENV === 'development'
