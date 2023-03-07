3225b3759a01baa6e4d4a2e1f78356d0b1589ed7
absidue <48293849+absidue@users.noreply.github.com>
2023-03-07 07:07:18 +0100

    Skip the exists check for the databases, as stat does it anyway (#3272)

--- src/datastores/index.js
+++ src/datastores/index.js
@@ -6,12 +6,13 @@ if (process.env.IS_ELECTRON_MAIN) {
   const { app } = require('electron')
   const { join } = require('path')
   // this code only runs in the electron main process, so hopefully using sync fs code here should be fine 😬
-  const { existsSync, statSync, realpathSync } = require('fs')
+  const { statSync, realpathSync } = require('fs')
   const userDataPath = app.getPath('userData') // This is based on the user's OS
   dbPath = (dbName) => {
     let path = join(userDataPath, `${dbName}.db`)
 
-    if (existsSync(path) && statSync(path).isSymbolicLink) {
+    // returns undefined if the path doesn't exist
+    if (statSync(path, { throwIfNoEntry: false })?.isSymbolicLink) {
       path = realpathSync(path)
     }
 
