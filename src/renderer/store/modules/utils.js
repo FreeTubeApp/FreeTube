29dd63bf543479cff137b48e6e65f2dffc1c8e71
absidue <48293849+absidue@users.noreply.github.com>
2023-03-07 21:02:11 +0100

    Handle invidious streams tab URL

--- src/renderer/store/modules/utils.js
+++ src/renderer/store/modules/utils.js
@@ -315,7 +315,7 @@ const actions = {
     let urlType = 'unknown'
 
     const channelPattern =
-      /^\/(?:(?:channel|user|c)\/)?(?<channelId>[^/]+)(?:\/(?<tab>join|featured|videos|live|playlists|about|community|channels))?\/?$/
+      /^\/(?:(?:channel|user|c)\/)?(?<channelId>[^/]+)(?:\/(?<tab>join|featured|videos|live|streams|playlists|about|community|channels))?\/?$/
 
     const typePatterns = new Map([
       ['playlist', /^(\/playlist\/?|\/embed(\/?videoseries)?)$/],
@@ -422,6 +422,7 @@ const actions = {
         let subPath = null
         switch (match.groups.tab) {
           case 'live':
+          case 'streams':
             subPath = 'live'
             break
           case 'playlists':
