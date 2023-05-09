34f483cce52de28a0afc225e23e91c50e689f78e
chunky programmer <78101139+ChunkyProgrammer@users.noreply.github.com>
2023-05-09 12:42:47 -0400

    format piped instances json
    Co-Authored-By: absidue <48293849+absidue@users.noreply.github.com>

--- _scripts/getPipedInstances.js
+++ _scripts/getPipedInstances.js
@@ -3,5 +3,5 @@ const url = 'https://piped-instances.kavin.rocks/'
 
 fetch(url).then(e => e.json()).then(res => {
   const data = res.map(e => e.api_url)
-  fs.writeFile('././static/piped-instances.json', JSON.stringify(data))
+  fs.writeFile('././static/piped-instances.json', JSON.stringify(data, null, 2))
 })
