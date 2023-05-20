3300118ff807a25e20e048936b43f82697bf083a
Chunky programmer <78101139+ChunkyProgrammer@users.noreply.github.com>
2023-05-20 10:55:58 -0400

    add piped to get instances script

--- _scripts/getInstances.js
+++ _scripts/getInstances.js
@@ -14,3 +14,17 @@ fetch(invidiousApiUrl).then(e => e.json()).then(res => {
   })
   fs.writeFile('././static/invidious-instances.json', JSON.stringify(data, null, 2))
 })
+
+const pipedApiUrl = 'https://piped-instances.kavin.rocks/'
+
+fetch(pipedApiUrl).then(e => e.json()).then(res => {
+  const data = res.map(e => e.api_url)
+  fs.writeFile('././static/piped-instances.json', JSON.stringify(data, null, 2))
+})
+
+const pipedApiUrl = 'https://piped-instances.kavin.rocks/'
+
+fetch(pipedApiUrl).then(e => e.json()).then(res => {
+  const data = res.map(e => e.api_url)
+  fs.writeFile('././static/piped-instances.json', JSON.stringify(data, null, 2))
+})
