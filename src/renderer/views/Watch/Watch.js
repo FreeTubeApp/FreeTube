29df27d1c49cbe80e5cb3f52c641bd3a1eeefb58
absidue <48293849+absidue@users.noreply.github.com>
2023-07-31 23:48:13 +0200

    Use emit and props instead of $parent

--- src/renderer/views/Watch/Watch.js
+++ src/renderer/views/Watch/Watch.js
@@ -256,9 +256,6 @@ export default defineComponent({
     changeTimestamp: function (timestamp) {
       this.$refs.videoPlayer.player.currentTime(timestamp)
     },
-    toggleTheatreMode: function () {
-      this.useTheatreMode = !this.useTheatreMode
-    },
 
     getVideoInformationLocal: async function () {
       if (this.firstLoad) {
@@ -1110,6 +1107,20 @@ export default defineComponent({
             this.getVideoInformationInvidious()
           }
         })
+    },
+
+    handleFormatChange: function (format) {
+      switch (format) {
+        case 'dash':
+          this.enableDashFormat()
+          break
+        case 'legacy':
+          this.enableLegacyFormat()
+          break
+        case 'audio':
+          this.enableAudioFormat()
+          break
+      }
     },
 
     enableDashFormat: function () {
