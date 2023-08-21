6647e83d6bcf06d33237495877809658a097c037
ChunkyProgrammer <78101139+ChunkyProgrammer@users.noreply.github.com>
2023-08-21 06:23:58 -0700

    fix getInstances.js

--- _scripts/getInstances.js
+++ _scripts/getInstances.js
@@ -21,10 +21,3 @@ fetch(pipedApiUrl).then(e => e.json()).then(res => {
   const data = res.map(e => e.api_url)
   fs.writeFile('././static/piped-instances.json', JSON.stringify(data, null, 2))
 })
-
-const pipedApiUrl = 'https://piped-instances.kavin.rocks/'
-
-fetch(pipedApiUrl).then(e => e.json()).then(res => {
-  const data = res.map(e => e.api_url)
-  fs.writeFile('././static/piped-instances.json', JSON.stringify(data, null, 2))
-})
