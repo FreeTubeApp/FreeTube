edb841b2c79db6220680dea72960b694cdd29232
PikachuEXE <pikachuexe@gmail.com>
2023-08-22 09:05:46 +0800

    * Use v-if instead of v-show

--- src/renderer/views/UserPlaylists/UserPlaylists.vue
+++ src/renderer/views/UserPlaylists/UserPlaylists.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
     <ft-loader
-      v-show="isLoading"
+      v-if="isLoading"
       :fullscreen="true"
     />
     <ft-card
-      v-show="!isLoading"
+      v-if="!isLoading"
       class="card"
     >
       <div class="heading">
@@ -20,7 +20,7 @@
           @click="createNewPlaylist"
         />
         <ft-input
-          v-show="fullData.length > 0"
+          v-if="fullData.length > 0"
           ref="searchBar"
           :placeholder="$t('User Playlists.Search bar placeholder')"
           :show-clear-text-button="true"
@@ -30,21 +30,21 @@
         />
       </div>
       <ft-flex-box
-        v-show="fullData.length === 0"
+        v-if="fullData.length === 0"
       >
         <p class="message">
           You have no playlist. Click on the create new playlist button to create a new one.
         </p>
       </ft-flex-box>
       <ft-flex-box
-        v-show="activeData.length === 0 && fullData.length > 0"
+        v-else-if="activeData.length === 0 && fullData.length > 0"
       >
         <p class="message">
           {{ $t("User Playlists['Empty Search Message']") }}
         </p>
       </ft-flex-box>
       <ft-element-list
-        v-if="activeData.length > 0 && !isLoading"
+        v-else-if="activeData.length > 0 && !isLoading"
         :data="activeData"
         :use-channels-hidden-preference="false"
       />
