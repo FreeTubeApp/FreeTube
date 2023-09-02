81f8dce1d5f87cdedd93c82cc1f79314f8ec2e58
Jason Henriquez <jasonhenriquez@protonmail.com>
2023-09-02 02:11:57 -0500

    Update styling to equalize list and grid styles for community posts

--- src/renderer/components/ft-community-post/ft-community-post.vue
+++ src/renderer/components/ft-community-post/ft-community-post.vue
@@ -5,86 +5,90 @@
     :appearance="appearance"
     :class="{ list: listType === 'list', grid: listType === 'grid' }"
   >
-    <div
-      class="author-div"
-    >
+    <div>
       <img
         v-if="authorThumbnails.length > 0"
         :src="getBestQualityImage(authorThumbnails)"
         class="communityThumbnail"
         alt=""
       >
+    </div>
+    <div>
+      <div
+        class="author-div"
+      >
+        <p
+          class="authorName"
+        >
+          {{ author }}
+        </p>
+        <p
+          class="publishedText"
+        >
+          {{ publishedText }}
+        </p>
+      </div>
       <p
-        class="authorName"
-      >
-        {{ author }}
-      </p>
-      <p
-        class="publishedText"
-      >
-        {{ publishedText }}
-      </p>
-    </div>
-    <p
-      class="postText"
-      v-html="postText"
-    />
-    <tiny-slider
-      v-if="type === 'multiImage' && postContent.content.length > 0"
-      v-bind="tinySliderOptions"
-      class="slider"
-    >
-      <img
-        v-for="(img, index) in postContent.content"
-        :key="index"
-        :src="getBestQualityImage(img)"
-        class="communityImage tns-lazy-img"
-        alt=""
-      >
-    </tiny-slider>
-    <div
-      v-if="type === 'image' && postContent.content.length > 0"
-    >
-      <img
-        :src="getBestQualityImage(postContent.content)"
-        class="communityImage"
-        alt=""
-      >
-    </div>
-    <div
-      v-if="type === 'video'"
-    >
-      <ft-list-video
-        :data="data.postContent.content"
-        appearance=""
+        class="postText"
+        v-html="postText"
       />
-    </div>
-    <div
-      v-if="type === 'poll' || type === 'quiz'"
-    >
-      <ft-community-poll :data="postContent" />
-    </div>
-    <div
-      v-if="type === 'playlist'"
-      class="playlistWrapper"
-    >
-      <ft-list-playlist
-        :data="postContent.content"
-        :appearance="appearance"
-      />
-    </div>
-    <div
-      class="bottomSection"
-    >
-      <span class="likeCount"><font-awesome-icon
-        class="thumbs-up-icon"
-        :icon="['fas', 'thumbs-up']"
-      /> {{ voteCount }}</span>
-      <span class="commentCount">
-        <font-awesome-icon
-          class="comment-count-icon"
-          :icon="['fas', 'comment']"
-        /> {{ commentCount }}</span>
+      <tiny-slider
+        v-if="type === 'multiImage' && postContent.content.length > 0"
+        v-bind="tinySliderOptions"
+        class="slider"
+      >
+        <img
+          v-for="(img, index) in postContent.content"
+          :key="index"
+          :src="getBestQualityImage(img)"
+          class="communityImage tns-lazy-img"
+          alt=""
+        >
+      </tiny-slider>
+      <div
+        v-if="type === 'image' && postContent.content.length > 0"
+      >
+        <img
+          :src="getBestQualityImage(postContent.content)"
+          class="communityImage"
+          alt=""
+        >
+      </div>
+      <div
+        v-if="type === 'video'"
+      >
+        <ft-list-video
+          :data="data.postContent.content"
+          appearance=""
+        />
+      </div>
+      <div
+        v-if="type === 'poll' || type === 'quiz'"
+      >
+        <ft-community-poll :data="postContent" />
+      </div>
+      <div
+        v-if="type === 'playlist'"
+        class="playlistWrapper"
+      >
+        <ft-list-playlist
+          :data="postContent.content"
+          :appearance="appearance"
+        />
+      </div>
+      <div
+        class="bottomSection"
+      >
+        <span class="likeCount"><font-awesome-icon
+          class="thumbs-up-icon"
+          :icon="['fas', 'thumbs-up']"
+        /> {{ voteCount }}</span>
+        <span class="commentCount">
+          <font-awesome-icon
+            class="comment-count-icon"
+            :icon="['fas', 'comment']"
+          /> {{ commentCount }}</span>
+      </div>
     </div>
   </div>
 </template>
