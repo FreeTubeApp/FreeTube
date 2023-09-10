cdd4b99cb7bf4766c36b18920f4b99fbca993abf
Jason Henriquez <jasonhenriquez@protonmail.com>
2023-09-10 17:56:25 -0500

    Replace non-logical property usage in templates
    This is not enforced by the plugin at this time.

--- src/renderer/components/ft-list-video/ft-list-video.vue
+++ src/renderer/components/ft-list-video/ft-list-video.vue
@@ -66,7 +66,7 @@
       <div
         v-if="watched"
         class="watchedProgressBar"
-        :style="{width: progressPercentage + '%'}"
+        :style="{inlineSize: progressPercentage + '%'}"
       />
     </div>
     <div class="info">
