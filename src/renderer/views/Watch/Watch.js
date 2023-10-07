0c92b63b992fead8398749f678b5dc9b62accf5a
PikachuEXE <pikachuexe@gmail.com>
2023-10-08 05:15:51 +0800

    ! Fix watch page video published time parsing (#4105)

--- src/renderer/views/Watch/Watch.js
+++ src/renderer/views/Watch/Watch.js
@@ -322,7 +322,8 @@ export default defineComponent({
           channelId: this.channelId
         })
 
-        this.videoPublished = new Date(result.page[0].microformat.publish_date.replace('-', '/')).getTime()
+        // `result.page[0].microformat.publish_date` example value: `2023-08-12T08:59:59-07:00`
+        this.videoPublished = new Date(result.page[0].microformat.publish_date).getTime()
 
         if (result.secondary_info?.description.runs) {
           try {
