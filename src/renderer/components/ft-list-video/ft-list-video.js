86bad257f55d3ee75eb70804b66caaeb671ad006
Mitchel Paulin <Mitchel0022@gmail.com>
2023-11-20 21:04:41 -0500

    Add hide channel option directly to More Options menu (#4228)
    * Add hide channel option directly to More Options menu
    
    * Added ability to unhide channels as well from the same menu
    
    * Fix issue where unhide would not work with channelId
    
    * Replace filter with some
    
    * Remove extra hide channel
    
    * Hide by channelId rather than name
    
    * Shuffle lang terms to put channel name at the front
    
    * Do not check channel name anymore
    
    * One more spot
    
    * Fixed lang terms

--- src/renderer/components/ft-list-video/ft-list-video.js
+++ src/renderer/components/ft-list-video/ft-list-video.js
@@ -228,8 +228,25 @@ export default defineComponent({
             {
               label: this.$t('Video.Open Channel in Invidious'),
               value: 'openInvidiousChannel'
+            },
+            {
+              type: 'divider'
             }
           )
+
+          const hiddenChannels = JSON.parse(this.$store.getters.getChannelsHidden)
+          const channelShouldBeHidden = hiddenChannels.some(c => c === this.channelId)
+          if (channelShouldBeHidden) {
+            options.push({
+              label: this.$t('Video.Unhide Channel'),
+              value: 'unhideChannel'
+            })
+          } else {
+            options.push({
+              label: this.$t('Video.Hide Channel'),
+              value: 'hideChannel'
+            })
+          }
         }
       }
 
@@ -434,6 +451,12 @@ export default defineComponent({
         case 'openInvidiousChannel':
           openExternalLink(this.invidiousChannelUrl)
           break
+        case 'hideChannel':
+          this.hideChannel(this.channelName, this.channelId)
+          break
+        case 'unhideChannel':
+          this.unhideChannel(this.channelName, this.channelId)
+          break
       }
     },
 
@@ -621,12 +644,28 @@ export default defineComponent({
       showToast(this.$t('Video.Video has been removed from your saved list'))
     },
 
+    hideChannel: function(channelName, channelId) {
+      const hiddenChannels = JSON.parse(this.$store.getters.getChannelsHidden)
+      hiddenChannels.push(channelId)
+      this.updateChannelsHidden(JSON.stringify(hiddenChannels))
+
+      showToast(this.$t('Channel Hidden', { channel: channelName }))
+    },
+
+    unhideChannel: function(channelName, channelId) {
+      const hiddenChannels = JSON.parse(this.$store.getters.getChannelsHidden)
+      this.updateChannelsHidden(JSON.stringify(hiddenChannels.filter(c => c !== channelId)))
+
+      showToast(this.$t('Channel Unhidden', { channel: channelName }))
+    },
+
     ...mapActions([
       'openInExternalPlayer',
       'updateHistory',
       'removeFromHistory',
       'addVideo',
-      'removeVideo'
+      'removeVideo',
+      'updateChannelsHidden'
     ])
   }
 })
