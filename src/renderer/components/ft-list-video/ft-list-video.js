5b32efc8c62d2f36451bb4c4f1867cee8c60e0dd
absidue <48293849+absidue@users.noreply.github.com>
2024-01-02 16:38:56 +0100

    Cleanup shorts duration parsing now that YouTube has removed it

--- src/renderer/components/ft-list-video/ft-list-video.js
+++ src/renderer/components/ft-list-video/ft-list-video.js
@@ -64,6 +64,7 @@ export default defineComponent({
       viewCount: 0,
       parsedViewCount: '',
       uploadedTime: '',
+      lengthSeconds: 0,
       duration: '',
       description: '',
       watched: false,
@@ -156,7 +157,11 @@ export default defineComponent({
     },
 
     progressPercentage: function () {
-      return (this.watchProgress / this.data.lengthSeconds) * 100
+      if (typeof this.lengthSeconds !== 'number') {
+        return 0
+      }
+
+      return (this.watchProgress / this.lengthSeconds) * 100
     },
 
     hideSharingActions: function() {
@@ -468,9 +473,11 @@ export default defineComponent({
       this.channelName = this.data.author ?? null
       this.channelId = this.data.authorId ?? null
 
-      if (this.data.isRSS && this.historyEntryExists) {
+      if ((this.data.lengthSeconds === '' || this.data.lengthSeconds === '0:00') && this.historyEntryExists) {
+        this.lengthSeconds = this.historyEntry.lengthSeconds
         this.duration = formatDurationAsTimestamp(this.historyEntry.lengthSeconds)
       } else {
+        this.lengthSeconds = this.data.lengthSeconds
         this.duration = formatDurationAsTimestamp(this.data.lengthSeconds)
       }
 
