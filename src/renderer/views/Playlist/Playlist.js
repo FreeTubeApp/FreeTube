88a4c1fcc4b9520ce016d291b3e552f6bf925383
PikachuEXE <git@pikachuexe.net>
2024-01-23 14:35:45 +0800

    * Don't remember pagination limit, which reintroduce performance issue

--- src/renderer/views/Playlist/Playlist.js
+++ src/renderer/views/Playlist/Playlist.js
@@ -159,8 +159,6 @@ export default defineComponent({
       this.getPlaylistInfoDebounce()
     },
     userPlaylistVisibleVideoLimit (val) {
-      sessionStorage.setItem(`Playlist/userPlaylistVisibleVideoLimit/${this.playlistId}`, val)
-
       if (this.selectedUserPlaylistVideos.length < this.userPlaylistVisibleVideoLimit) {
         this.playlistItems = this.selectedUserPlaylistVideos
       } else {
@@ -169,11 +167,6 @@ export default defineComponent({
     },
   },
   mounted: function () {
-    const limit = sessionStorage.getItem(`Playlist/userPlaylistVisibleVideoLimit/${this.playlistId}`)
-    if (limit !== null) {
-      this.userPlaylistVisibleVideoLimit = limit
-    }
-
     this.getPlaylistInfoDebounce = debounce(this.getPlaylistInfo, 100)
     this.getPlaylistInfoDebounce()
   },
