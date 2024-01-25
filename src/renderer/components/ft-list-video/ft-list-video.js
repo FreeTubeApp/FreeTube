e560a8fb60427527938af1db35ad654ff9078581
PikachuEXE <pikachuexe@gmail.com>
2024-01-25 21:37:35 +0800

    Update places generating sharable YT video URLs to always return prefix https://youtu.be/ (#4547)
    * * Update places generating sharable YT video URLs to always return prefix https://youtu.be/
    
    * ! Fix short form URL

--- src/renderer/components/ft-list-video/ft-list-video.js
+++ src/renderer/components/ft-list-video/ft-list-video.js
@@ -192,11 +192,12 @@ export default defineComponent({
     },
 
     youtubeShareUrl: function () {
+      const videoUrl = `https://youtu.be/${this.id}`
       if (this.playlistSharable) {
         // `index` seems can be ignored
-        return `https://youtu.be/${this.id}?list=${this.playlistIdFinal}`
+        return `${videoUrl}?list=${this.playlistIdFinal}`
       }
-      return `https://youtu.be/${this.id}`
+      return videoUrl
     },
 
     youtubeChannelUrl: function () {
