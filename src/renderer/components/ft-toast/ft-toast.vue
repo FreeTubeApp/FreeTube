064170be224297a7ec165fd7c1a080498c11cb58
absidue <48293849+absidue@users.noreply.github.com>
2024-03-07 15:16:53 +0100

    Fix visible toast messages flickering when one disappears (#4749)

--- src/renderer/components/ft-toast/ft-toast.vue
+++ src/renderer/components/ft-toast/ft-toast.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="toast-holder">
     <div
-      v-for="(toast, index) in toasts"
-      :key="'toast-' + index"
+      v-for="toast in toasts"
+      :key="toast.id"
       class="toast"
       :class="{ closed: !toast.isOpen, open: toast.isOpen }"
       tabindex="0"
       role="status"
-      @click="performAction(index)"
-      @keydown.enter.prevent="performAction(index)"
-      @keydown.space.prevent="performAction(index)"
+      @click="performAction(toast.id)"
+      @keydown.enter.prevent="performAction(toast.id)"
+      @keydown.space.prevent="performAction(toast.id)"
     >
       <p class="message">
         {{ toast.message }}
