5633ad90379dc2cfc48905c839b2b5963662e1cd
Emma <MarmadileManteater@proton.me>
2024-04-10 22:18:51 -0400

    Make thumbnail icons very slightly larger

--- src/renderer/components/ft-list-video/ft-list-video.vue
+++ src/renderer/components/ft-list-video/ft-list-video.vue
@@ -52,7 +52,7 @@
           class="addToPlaylistIcon"
           :class="alwaysShowAddToPlaylistButton ? 'alwaysVisible' : ''"
           :padding="appearance === `watchPlaylistItem` ? 5 : 6"
-          :size="appearance === `watchPlaylistItem` ? 14 : 18"
+          :size="appearance === `watchPlaylistItem` ? 14 : 22"
           @click="togglePlaylistPrompt"
         />
         <ft-icon-button
@@ -66,7 +66,7 @@
           }"
           :theme="quickBookmarkIconTheme"
           :padding="appearance === `watchPlaylistItem` ? 5 : 6"
-          :size="appearance === `watchPlaylistItem` ? 14 : 18"
+          :size="appearance === `watchPlaylistItem` ? 14 : 22"
           @click="toggleQuickBookmarked"
         />
         <ft-icon-button
