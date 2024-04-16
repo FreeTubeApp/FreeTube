9815ed3da2e07b17330cf88d521096520a738fd3
Jason <84899178+jasonhenriquez@users.noreply.github.com>
2024-04-16 22:26:43 +0000

    Sort videos within playlist (#4921)
    * Implement playlist sorting
    
    * Hide sort menu for remote playlists
    
    * Remove 'Custom (descending)' sort order
    
    I don't see the need for this particular sort order.
    
    * Adjust sort order & align dropdown with 'More Options' button
    
    * Make 'Latest added first' default option instead of custom
    
    * Remove unlikely-to-be-implemented 'Date published' sorting options
    
    Context from absidue: 'I don't think we should even attempt to support it, due to all of the situations where it wouldn't be possible.'
    
    * Update to use sortOrder as main variable throughout
    
    * Hide sort menu for playlists of length <2

--- src/renderer/views/Playlist/Playlist.js
+++ src/renderer/views/Playlist/Playlist.js
@@ -7,6 +7,7 @@ import PlaylistInfo from '../../components/playlist-info/playlist-info.vue'
 import FtListVideoNumbered from '../../components/ft-list-video-numbered/ft-list-video-numbered.vue'
 import FtFlexBox from '../../components/ft-flex-box/ft-flex-box.vue'
 import FtButton from '../../components/ft-button/ft-button.vue'
+import FtSelect from '../../components/ft-select/ft-select.vue'
 import FtAutoLoadNextPageWrapper from '../../components/ft-auto-load-next-page-wrapper/ft-auto-load-next-page-wrapper.vue'
 import {
   getLocalPlaylist,
@@ -15,6 +16,16 @@ import {
 } from '../../helpers/api/local'
 import { extractNumberFromString, setPublishedTimestampsInvidious, showToast } from '../../helpers/utils'
 import { invidiousGetPlaylistInfo, youtubeImageUrlToInvidious } from '../../helpers/api/invidious'
+
+const SORT_BY_VALUES = {
+  DateAddedNewest: 'date_added_descending',
+  DateAddedOldest: 'date_added_ascending',
+  AuthorAscending: 'author_ascending',
+  AuthorDescending: 'author_descending',
+  VideoTitleAscending: 'video_title_ascending',
+  VideoTitleDescending: 'video_title_descending',
+  Custom: 'custom',
+}
 
 export default defineComponent({
   name: 'Playlist',
@@ -25,6 +36,7 @@ export default defineComponent({
     'ft-list-video-numbered': FtListVideoNumbered,
     'ft-flex-box': FtFlexBox,
     'ft-button': FtButton,
+    'ft-select': FtSelect,
     'ft-auto-load-next-page-wrapper': FtAutoLoadNextPageWrapper,
   },
   beforeRouteLeave(to, from, next) {
@@ -76,6 +88,12 @@ export default defineComponent({
     },
     currentInvidiousInstance: function () {
       return this.$store.getters.getCurrentInvidiousInstance
+    },
+    userPlaylistSortOrder: function () {
+      return this.$store.getters.getUserPlaylistSortOrder
+    },
+    sortOrder: function () {
+      return this.isUserPlaylistRequested ? this.userPlaylistSortOrder : SORT_BY_VALUES.Custom
     },
     currentLocale: function () {
       return this.$i18n.locale.replace('_', '-')
@@ -138,10 +156,10 @@ export default defineComponent({
     },
 
     sometimesFilteredUserPlaylistItems() {
-      if (!this.isUserPlaylistRequested) { return this.playlistItems }
-      if (this.processedVideoSearchQuery === '') { return this.playlistItems }
+      if (!this.isUserPlaylistRequested) { return this.sortedPlaylistItems }
+      if (this.processedVideoSearchQuery === '') { return this.sortedPlaylistItems }
 
-      return this.playlistItems.filter((v) => {
+      return this.sortedPlaylistItems.filter((v) => {
         if (typeof (v.title) === 'string' && v.title.toLowerCase().includes(this.processedVideoSearchQuery)) {
           return true
         } else if (typeof (v.author) === 'string' && v.author.toLowerCase().includes(this.processedVideoSearchQuery)) {
@@ -151,10 +169,41 @@ export default defineComponent({
         return false
       })
     },
+    sortByValues() {
+      return Object.values(SORT_BY_VALUES)
+    },
+    isSortOrderCustom() {
+      return this.sortOrder === SORT_BY_VALUES.Custom
+    },
+    sortedPlaylistItems: function () {
+      if (this.sortOrder === SORT_BY_VALUES.Custom) {
+        return this.playlistItems
+      }
+
+      return this.playlistItems.toSorted((a, b) => {
+        switch (this.sortOrder) {
+          case SORT_BY_VALUES.DateAddedNewest:
+            return b.timeAdded - a.timeAdded
+          case SORT_BY_VALUES.DateAddedOldest:
+            return a.timeAdded - b.timeAdded
+          case SORT_BY_VALUES.VideoTitleAscending:
+            return a.title.localeCompare(b.title, this.currentLocale)
+          case SORT_BY_VALUES.VideoTitleDescending:
+            return b.title.localeCompare(a.title, this.currentLocale)
+          case SORT_BY_VALUES.AuthorAscending:
+            return a.author.localeCompare(b.author, this.currentLocale)
+          case SORT_BY_VALUES.AuthorDescending:
+            return b.author.localeCompare(a.author, this.currentLocale)
+          default:
+            console.error(`Unknown sortOrder: ${this.sortOrder}`)
+            return 0
+        }
+      })
+    },
     visiblePlaylistItems: function () {
       if (!this.isUserPlaylistRequested) {
         // No filtering for non user playlists yet
-        return this.playlistItems
+        return this.sortedPlaylistItems
       }
 
       if (this.userPlaylistVisibleLimit < this.sometimesFilteredUserPlaylistItems.length) {
@@ -165,6 +214,32 @@ export default defineComponent({
     },
     processedVideoSearchQuery() {
       return this.videoSearchQuery.trim().toLowerCase()
+    },
+    sortBySelectNames() {
+      return this.sortByValues.map((k) => {
+        switch (k) {
+          case SORT_BY_VALUES.Custom:
+            return this.$t('Playlist.Sort By.Custom')
+          case SORT_BY_VALUES.DateAddedNewest:
+            return this.$t('Playlist.Sort By.DateAddedNewest')
+          case SORT_BY_VALUES.DateAddedOldest:
+            return this.$t('Playlist.Sort By.DateAddedOldest')
+          case SORT_BY_VALUES.VideoTitleAscending:
+            return this.$t('Playlist.Sort By.VideoTitleAscending')
+          case SORT_BY_VALUES.VideoTitleDescending:
+            return this.$t('Playlist.Sort By.VideoTitleDescending')
+          case SORT_BY_VALUES.AuthorAscending:
+            return this.$t('Playlist.Sort By.AuthorAscending')
+          case SORT_BY_VALUES.AuthorDescending:
+            return this.$t('Playlist.Sort By.AuthorDescending')
+          default:
+            console.error(`Unknown sort: ${k}`)
+            return k
+        }
+      })
+    },
+    sortBySelectValues() {
+      return this.sortByValues
     },
   },
   watch: {
@@ -485,6 +560,7 @@ export default defineComponent({
     ...mapActions([
       'updateSubscriptionDetails',
       'updatePlaylist',
+      'updateUserPlaylistSortOrder',
       'removeVideo',
     ]),
 
