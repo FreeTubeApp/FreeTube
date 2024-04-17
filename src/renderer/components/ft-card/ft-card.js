6f6de879fe33190194f888b1240d35a3c3292ace
ChunkyProgrammer <78101139+ChunkyProgrammer@users.noreply.github.com>
2024-04-17 18:17:10 -0400

    remove ft-card changes since they're no longer needed

--- src/renderer/components/ft-card/ft-card.js
+++ src/renderer/components/ft-card/ft-card.js
@@ -1,11 +1,5 @@
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: 'FtCard',
-  emits: ['focusout'],
-  methods: {
-    focusOut: function () {
-      this.$emit('focusout')
-    }
-  }
+  name: 'FtCard'
 })
