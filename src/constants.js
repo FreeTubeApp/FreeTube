3a5bc09d6a9075ed5fced2376511e0c6e6a8f601
P.S <phil_song@hotmail.com>
2024-04-29 20:58:50 +1000

    Update comment based on review

--- src/constants.js
+++ src/constants.js
@@ -76,7 +76,7 @@ const SyncEvents = {
 // Utils
 const MAIN_PROFILE_ID = 'allChannels'
 
-// Google search character limit is 100 characters
+// YouTube search character limit is 100 characters
 const SEARCH_CHAR_LIMIT = 100
 
 export {
