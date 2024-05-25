2e43b09bf0fda21ef6e7f995307a85748285db44
absidue <48293849+absidue@users.noreply.github.com>
2024-05-25 12:30:42 +0200

    Use addVideo instead of addVideos for quick bookmark button (#5168)

--- src/renderer/components/ft-list-video/ft-list-video.js
+++ src/renderer/components/ft-list-video/ft-list-video.js
@@ -783,14 +783,12 @@ export default defineComponent({
         title: this.title,
         author: this.channelName,
         authorId: this.channelId,
-        description: this.description,
-        viewCount: this.viewCount,
         lengthSeconds: this.data.lengthSeconds,
       }
 
-      this.addVideos({
+      this.addVideo({
         _id: this.quickBookmarkPlaylist._id,
-        videos: [videoData],
+        videoData,
       })
       // Update playlist's `lastUpdatedAt`
       this.updatePlaylist({ _id: this.quickBookmarkPlaylist._id })
@@ -828,7 +826,7 @@ export default defineComponent({
       'removeFromHistory',
       'updateChannelsHidden',
       'showAddToPlaylistPromptForManyVideos',
-      'addVideos',
+      'addVideo',
       'updatePlaylist',
       'removeVideo',
     ])
