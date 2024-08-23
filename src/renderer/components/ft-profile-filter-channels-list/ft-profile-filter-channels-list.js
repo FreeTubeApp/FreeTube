579d6d397db6bf8884155bf40fd5a4837c295b06
absidue <48293849+absidue@users.noreply.github.com>
2024-08-23 17:18:49 +0200

    Support authentication with the Basic scheme for Invidious instances (#5569)
    * Support authentication with the Basic scheme for Invidious instances
    
    * I forgot to change this line

--- src/renderer/components/ft-profile-filter-channels-list/ft-profile-filter-channels-list.js
+++ src/renderer/components/ft-profile-filter-channels-list/ft-profile-filter-channels-list.js
@@ -36,8 +36,8 @@ export default defineComponent({
     backendPreference: function () {
       return this.$store.getters.getBackendPreference
     },
-    currentInvidiousInstance: function () {
-      return this.$store.getters.getCurrentInvidiousInstance
+    currentInvidiousInstanceUrl: function () {
+      return this.$store.getters.getCurrentInvidiousInstanceUrl
     },
     profileList: function () {
       return this.$store.getters.getProfileList
@@ -71,7 +71,7 @@ export default defineComponent({
         return index === -1
       }).map((channel) => {
         if (this.backendPreference === 'invidious') {
-          channel.thumbnail = youtubeImageUrlToInvidious(channel.thumbnail, this.currentInvidiousInstance)
+          channel.thumbnail = youtubeImageUrlToInvidious(channel.thumbnail, this.currentInvidiousInstanceUrl)
         }
         channel.selected = false
         return channel
@@ -92,7 +92,7 @@ export default defineComponent({
         return index === -1
       }).map((channel) => {
         if (this.backendPreference === 'invidious') {
-          channel.thumbnail = youtubeImageUrlToInvidious(channel.thumbnail, this.currentInvidiousInstance)
+          channel.thumbnail = youtubeImageUrlToInvidious(channel.thumbnail, this.currentInvidiousInstanceUrl)
         }
         channel.selected = false
         return channel
