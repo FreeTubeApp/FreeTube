008daface4fcc2151912b415cbbcfbf3929ff624
absidue <48293849+absidue@users.noreply.github.com>
2024-09-28 16:03:43 +0200

    Let translateWindowTitle use the imported i18n instance (#5745)

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,9 +1,9 @@
 // import the styles
 import Vue from 'vue'
-import App from './App.vue'
+import i18n from './i18n/index'
 import router from './router/index'
 import store from './store/index'
-import i18n from './i18n/index'
+import App from './App.vue'
 import { IpcChannels } from '../constants'
 import { library } from '@fortawesome/fontawesome-svg-core'
 
