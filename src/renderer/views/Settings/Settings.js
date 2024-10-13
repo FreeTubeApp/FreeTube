98df2108066bf725c8c60bea40697b2c739b13a0
Jason <84899178+kommunarr@users.noreply.github.com>
2024-10-13 07:16:00 +0000

    Settings menu & UX improvements (#5029)
    * Implement settings section sorting
    
    * Make IS_ELECTRON property used directly
    
    * Implement settings menu with icons
    
    * Implement short labels and fallback to longer label when translation doesn't exist
    
    * Implement dynamic scroll-based underlining of active section
    
    * Ensure General Settings is always first, ignoring sorting
    
    Solving the problem of preventing 'jumping around' when changing locales with alphabetical sorting is hard. This problem is easily solved by stickying general settings to the top of the list unconditionally. This is in line with ChunkyProgrammer's initial assessment of how to improve the settings order in https://github.com/FreeTubeApp/FreeTube/issues/1739, albeit not also moving Theme Settings to the top for the time being. The rationale from a functional level is that General Settings is a hub. Even when you change languages, change sort order, or what have you, General Settings is right at the top. I don't imagine we need to update the label of the setting, as I think this relationship is quite intuitive.
    
    * Styling & documentation improvements; remove problematized Hot Pink link underline removal experiment
    
    * Implement bare minimum mobile styling
    
    * Increase link padding
    
    * Fix underline to be equal on both sides
    
    * Update to have first section active by default
    
    * Make constant for 'active' class name
    
    * Replace proxy icon with more fitting one
    
    * Move shortTitle logic
    
    * Update scrolling logic to not use anchor links
    
    * Implement mobile view for Settings Menu
    
    * Replace rounded corners and move section titles to the interior
    
    * Update font sizes and breakpoints to work for all supported devices & languages
    
    * Update as per review comments
    
    * Make class-specific constants declared in file
    
    * Update settings menu link sizing to be dynamic to better accommodate mobile/tablet devices
    
    * Focus active menu on mobile close
    
    * Replace shortTitle with title, & replace EN-US labels with shorter forms
    
    * Update menu font size for mobile devices
    
    * Focus section heading on navigating to it in desktop view
    
    ---------
    
    Co-authored-by: Jason <84899178+jasonhenriquez@users.noreply.github.com>

--- src/renderer/views/Settings/Settings.js
+++ src/renderer/views/Settings/Settings.js
@@ -1,4 +1,4 @@
-import { defineComponent } from 'vue'
+import { defineComponent, nextTick } from 'vue'
 import { mapActions } from 'vuex'
 import GeneralSettings from '../../components/general-settings/general-settings.vue'
 import ThemeSettings from '../../components/theme-settings/theme-settings.vue'
@@ -16,6 +16,10 @@ import ExperimentalSettings from '../../components/experimental-settings/experim
 import PasswordSettings from '../../components/password-settings/password-settings.vue'
 import PasswordDialog from '../../components/password-dialog/password-dialog.vue'
 import FtToggleSwitch from '../../components/ft-toggle-switch/ft-toggle-switch.vue'
+import FtSettingsMenu from '../../components/ft-settings-menu/ft-settings-menu.vue'
+
+const ACTIVE_CLASS_NAME = 'active'
+const SETTINGS_MOBILE_WIDTH_THRESHOLD = 1015
 
 export default defineComponent({
   name: 'Settings',
@@ -32,7 +36,7 @@ export default defineComponent({
     'password-settings': PasswordSettings,
     'password-dialog': PasswordDialog,
     'ft-toggle-switch': FtToggleSwitch,
-
+    'ft-settings-menu': FtSettingsMenu,
     ...(process.env.IS_ELECTRON
       ? {
           'proxy-settings': ProxySettings,
@@ -44,77 +48,9 @@ export default defineComponent({
   },
   data: function () {
     return {
-      unlocked: false,
-      settingsComponentsData: [
-        {
-          type: 'general-settings',
-          title: this.$t('Settings.General Settings.General Settings')
-        },
-        {
-          type: 'theme-settings',
-          title: this.$t('Settings.Theme Settings.Theme Settings')
-        },
-        {
-          type: 'player-settings',
-          title: this.$t('Settings.Player Settings.Player Settings')
-        },
-        ...(process.env.IS_ELECTRON
-          ? [
-              {
-                type: 'external-player-settings',
-                title: this.$t('Settings.External Player Settings.External Player Settings')
-              }
-            ]
-          : []),
-        {
-          type: 'subscription-settings',
-          title: this.$t('Settings.Subscription Settings.Subscription Settings')
-        },
-        {
-          type: 'distraction-settings',
-          title: this.$t('Settings.Distraction Free Settings.Distraction Free Settings')
-        },
-        {
-          type: 'privacy-settings',
-          title: this.$t('Settings.Privacy Settings.Privacy Settings')
-        },
-        {
-          type: 'data-settings',
-          title: this.$t('Settings.Data Settings.Data Settings')
-        },
-        ...(process.env.IS_ELECTRON
-          ? [
-              {
-                type: 'proxy-settings',
-                title: this.$t('Settings.Proxy Settings.Proxy Settings')
-              },
-              {
-                type: 'download-settings',
-                title: this.$t('Settings.Download Settings.Download Settings')
-              }
-            ]
-          : []),
-        {
-          type: 'parental-control-settings',
-          title: this.$t('Settings.Parental Control Settings.Parental Control Settings')
-        },
-        {
-          type: 'sponsor-block-settings',
-          title: this.$t('Settings.SponsorBlock Settings.SponsorBlock Settings'),
-        },
-        ...(process.env.IS_ELECTRON
-          ? [
-              {
-                type: 'experimental-settings',
-                title: this.$t('Settings.Experimental Settings.Experimental Settings')
-              },
-            ]
-          : []),
-        {
-          type: 'password-settings',
-          title: this.$t('Settings.Password Settings.Password Settings')
-        },
-      ]
+      isInDesktopView: true,
+      settingsSectionTypeOpenInMobile: null,
+      unlocked: false
     }
   },
   computed: {
@@ -126,22 +62,106 @@ export default defineComponent({
       return this.$store.getters.getSettingsPassword
     },
 
-    allSettingsSectionsExpandedByDefault: function () {
-      return this.$store.getters.getAllSettingsSectionsExpandedByDefault
-    },
-
     settingsSectionSortEnabled: function () {
       return this.$store.getters.getSettingsSectionSortEnabled
     },
 
+    settingsComponentsData: function () {
+      const settingsComponentsData = [
+        {
+          type: 'theme-settings',
+          title: this.$t('Settings.Theme Settings.Theme Settings'),
+          icon: 'display'
+        },
+        {
+          type: 'player-settings',
+          title: this.$t('Settings.Player Settings.Player Settings'),
+          icon: 'circle-play'
+        },
+        ...(process.env.IS_ELECTRON
+          ? [{
+              type: 'external-player-settings',
+              title: this.$t('Settings.External Player Settings.External Player Settings'),
+              icon: 'clapperboard'
+            }]
+          : []),
+        {
+          type: 'subscription-settings',
+          title: this.$t('Settings.Subscription Settings.Subscription Settings'),
+          icon: 'play'
+        },
+        {
+          type: 'distraction-settings',
+          title: this.$t('Settings.Distraction Free Settings.Distraction Free Settings'),
+          icon: 'eye-slash'
+        },
+        {
+          type: 'privacy-settings',
+          title: this.$t('Settings.Privacy Settings.Privacy Settings'),
+          icon: 'lock'
+        },
+        {
+          type: 'data-settings',
+          title: this.$t('Settings.Data Settings.Data Settings'),
+          icon: 'database'
+        },
+        ...(process.env.IS_ELECTRON
+          ? [
+              {
+                type: 'proxy-settings',
+                title: this.$t('Settings.Proxy Settings.Proxy Settings'),
+                icon: 'network-wired',
+              },
+              {
+                type: 'download-settings',
+                title: this.$t('Settings.Download Settings.Download Settings'),
+                icon: 'download',
+              }
+            ]
+          : []),
+        {
+          type: 'parental-control-settings',
+          title: this.$t('Settings.Parental Control Settings.Parental Control Settings'),
+          icon: 'user-lock'
+        },
+        {
+          type: 'sponsor-block-settings',
+          title: this.$t('Settings.SponsorBlock Settings.SponsorBlock Settings'),
+          // TODO: replace with SponsorBlock icon
+          icon: 'shield'
+        },
+        ...(process.env.IS_ELECTRON
+          ? [{
+              type: 'experimental-settings',
+              title: this.$t('Settings.Experimental Settings.Experimental Settings'),
+              icon: 'flask'
+            }]
+          : []),
+        {
+          type: 'password-settings',
+          title: this.$t('Settings.Password Settings.Password Settings'),
+          icon: 'key'
+        },
+      ]
+      return settingsComponentsData
+    },
+
     settingsSectionComponents: function () {
+      let settingsSections = this.settingsComponentsData
       if (this.settingsSectionSortEnabled) {
-        return this.settingsComponentsData.toSorted((a, b) =>
-          a.title.toLowerCase().localeCompare(b.title.toLowerCase(), this.locale)
-        )
+        settingsSections = settingsSections.toSorted((a, b) => {
+          return a.title.toLowerCase().localeCompare(b.title.toLowerCase(), this.locale)
+        })
       }
 
-      return this.settingsComponentsData
+      // ensure General Settings is placed first regardless of sorting
+      const generalSettingsEntry = {
+        type: 'general-settings',
+        title: this.$t('Settings.General Settings.General Settings'),
+        icon: 'border-all'
+      }
+
+      return [generalSettingsEntry, ...settingsSections]
     },
   },
   created: function () {
@@ -149,9 +169,84 @@ export default defineComponent({
       this.unlocked = true
     }
   },
+  mounted: function () {
+    this.handleResize()
+    window.addEventListener('resize', this.handleResize)
+    document.addEventListener('scroll', this.markScrolledToSectionAsActive)
+
+    // mark first section as active before any scrolling has taken place
+    if (this.settingsSectionComponents.length > 0) {
+      const firstSection = document.getElementById(this.settingsSectionComponents[0].type)
+      firstSection.classList.add(ACTIVE_CLASS_NAME)
+    }
+  },
+  beforeDestroy: function () {
+    document.removeEventListener('scroll', this.markScrolledToSectionAsActive)
+    window.removeEventListener('resize', this.handleResize)
+  },
   methods: {
+    navigateToSection: function(sectionType) {
+      if (this.isInDesktopView) {
+        nextTick(() => {
+          const sectionElement = this.$refs[sectionType][0].$el
+          sectionElement.scrollIntoView()
+
+          const sectionHeading = sectionElement.firstChild.firstChild
+          sectionHeading.tabIndex = 0
+          sectionHeading.focus()
+          sectionHeading.tabIndex = -1
+        })
+      } else {
+        this.settingsSectionTypeOpenInMobile = sectionType
+      }
+    },
+
+    returnToSettingsMenu: function () {
+      const openSection = this.settingsSectionTypeOpenInMobile
+      this.settingsSectionTypeOpenInMobile = null
+
+      // focus the corresponding Settings Menu title
+      nextTick(() => document.getElementById(openSection)?.focus())
+    },
+
+    /* Set the current section to be shown as active in the Settings Menu
+    * if it is the lowest section within the top quarter of the viewport (25vh) */
+    markScrolledToSectionAsActive: function() {
+      const scrollY = window.scrollY + innerHeight / 4
+      this.settingsSectionComponents.forEach((section) => {
+        const sectionElement = this.$refs[section.type][0].$el
+        const sectionHeight = sectionElement.offsetHeight
+        const sectionTop = sectionElement.offsetTop
+        const correspondingMenuLink = document.getElementById(section.type)
+
+        if (this.isInDesktopView && scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+          correspondingMenuLink.classList.add(ACTIVE_CLASS_NAME)
+        } else {
+          correspondingMenuLink.classList.remove(ACTIVE_CLASS_NAME)
+        }
+      })
+    },
+
+    handleResize: function () {
+      const wasNotInDesktopView = !this.isInDesktopView
+      this.isInDesktopView = window.innerWidth > SETTINGS_MOBILE_WIDTH_THRESHOLD
+
+      // navigate to section that was open in mobile or desktop view, if any
+      if (this.isInDesktopView && wasNotInDesktopView && this.settingsSectionTypeOpenInMobile != null) {
+        this.navigateToSection(this.settingsSectionTypeOpenInMobile)
+        this.settingsSectionTypeOpenInMobile = null
+      } else if (!this.isInDesktopView && !wasNotInDesktopView) {
+        const activeMenuLink = document.querySelector(`.settingsMenu .title.${ACTIVE_CLASS_NAME}`)
+        if (!activeMenuLink) {
+          return
+        }
+
+        const sectionType = activeMenuLink.id
+        this.navigateToSection(sectionType)
+      }
+    },
+
     ...mapActions([
-      'updateAllSettingsSectionsExpandedByDefault',
       'updateSettingsSectionSortEnabled'
     ])
   }
