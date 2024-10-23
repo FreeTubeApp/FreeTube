df7add5bf0cbcd29f88679dde3708243a292f9bc
DontBlameMe99 <167469818+DontBlameMe99@users.noreply.github.com>
2024-10-23 12:48:24 +0200

    Remove unintentional formatting
    Removes the unintentional formatting, automatically done by my nvim lsp.
    This adds the blank space back in.

--- src/main/index.js
+++ src/main/index.js
@@ -455,7 +455,7 @@ function runApp() {
           requestHeaders.Authorization = invidiousAuthorization.authorization
         }
       }
-
+       
       callback({ requestHeaders })
     })
 
@@ -466,7 +466,7 @@ function runApp() {
       if (responseHeaders) {
         delete responseHeaders['set-cookie']
       }
-
+       
       callback({ responseHeaders })
     })
 
@@ -761,7 +761,7 @@ function runApp() {
     // If called multiple times
     // Duplicate menu items will be added
     if (replaceMainWindow) {
-
+       
       setMenu()
     }
 
