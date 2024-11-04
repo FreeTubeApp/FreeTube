c178c9c339535428984206c097dfab0817955b07
absidue <48293849+absidue@users.noreply.github.com>
2024-11-04 20:12:47 +0100

    Fix search box crash (#6085)

--- src/renderer/components/ft-input/ft-input.js
+++ src/renderer/components/ft-input/ft-input.js
@@ -100,10 +100,6 @@ export default defineComponent({
       return this.isSearch && this.barColor
     },
 
-    idDataList: function () {
-      return `${this.id}_datalist`
-    },
-
     inputDataPresent: function () {
       return this.inputData.length > 0
     },
