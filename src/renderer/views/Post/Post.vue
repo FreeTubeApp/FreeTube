2b4f7620dc82817124d3c90c9a7c3c8333513c6b
ChunkyProgrammer <78101139+ChunkyProgrammer@users.noreply.github.com>
2024-11-10 11:15:00 -0500

    Migrate Post view to Composition API

--- src/renderer/views/Post/Post.vue
+++ src/renderer/views/Post/Post.vue
@@ -28,4 +28,60 @@
   </div>
 </template>
 
-<script src="./Post.js" />
+<script setup>
+import { computed, onMounted, ref } from 'vue'
+import FtCard from '../../components/ft-card/ft-card.vue'
+import FtCommunityPost from '../../components/FtCommunityPost/FtCommunityPost.vue'
+import FtLoader from '../../components/ft-loader/ft-loader.vue'
+import WatchVideoComments from '../../components/watch-video-comments/watch-video-comments.vue'
+import store from '../../store/index'
+import { useRoute, useRouter } from 'vue-router/composables'
+const router = useRouter()
+const route = useRoute()
+import { getInvidiousCommunityPost } from '../../helpers/api/invidious'
+
+const id = ref('')
+const authorId = ref('')
+const post = ref(null)
+const isLoading = ref(true)
+
+/** @type {import('vue').ComputedRef<'invidious' | 'local'>} */
+const backendPreference = computed(() => {
+  return store.getters.getBackendPreference
+})
+
+/** @type {import('vue').ComputedRef<boolean>} */
+const backendFallback = computed(() => {
+  return store.getters.getBackendFallback
+})
+
+const isInvidiousAllowed = computed(() => {
+  return backendPreference.value === 'invidious' || backendFallback.value
+})
+
+onMounted(async () => {
+  if (isInvidiousAllowed.value) {
+    id.value = route.params.id
+    authorId.value = route.query.authorId
+    await loadDataInvidiousAsync()
+  }
+})
+
+async function loadDataInvidiousAsync() {
+  post.value = await getInvidiousCommunityPost(id.value, authorId.value)
+  authorId.value = post.value.authorId
+  isLoading.value = false
+
+  // If the authorId is missing from the URL we should add it,
+  // that way if the user comes back to this page by pressing the back button
+  // we don't have to resolve the authorId again
+  if (authorId.value !== route.query.authorId) {
+    router.replace({
+      path: `/post/${id.value}`,
+      query: {
+        authorId: authorId.value
+      }
+    })
+  }
+}
+</script>
