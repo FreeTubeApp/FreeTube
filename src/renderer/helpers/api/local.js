c448b3c85f94580bea1e6261a00ba031de3fc194
absidue <48293849+absidue@users.noreply.github.com>
2024-12-06 00:01:07 +0100

    Filter out unsupported watch page only mixes from search results (#6244)

--- src/renderer/helpers/api/local.js
+++ src/renderer/helpers/api/local.js
@@ -859,6 +859,7 @@ function handleSearchResponse(response) {
       return item.type === 'Video' || item.type === 'Channel' || item.type === 'Playlist' || item.type === 'HashtagTile' || item.type === 'Movie' || item.type === 'LockupView'
     })
     .map((item) => parseListItem(item))
+    .filter((item) => item)
 
   return {
     results,
@@ -1160,6 +1161,14 @@ function parseLockupView(lockupView, channelId = undefined, channelName = undefi
       const thumbnailOverlayBadgeView = lockupView.content_image.primary_thumbnail.overlays
         .find(overlay => overlay.is(YTNodes.ThumbnailOverlayBadgeView))
 
+      const playlistId = lockupView.content_id
+
+      // Filter out mixes without playlist pages (we don't support watch page-only mixes)
+      // https://wiki.archiveteam.org/index.php/YouTube/Technical_details#Playlists
+      if (playlistId.startsWith('RD') && !playlistId.startsWith('RDCL')) {
+        return null
+      }
+
       const maybeChannelText = lockupView.metadata?.metadata?.metadata_rows?.[0]?.metadata_parts?.[0]?.text
 
       if (maybeChannelText && maybeChannelText.endpoint?.metadata.page_type === 'WEB_PAGE_TYPE_CHANNEL') {
@@ -1170,7 +1179,7 @@ function parseLockupView(lockupView, channelId = undefined, channelName = undefi
       return {
         type: 'playlist',
         dataSource: 'local',
-        playlistId: lockupView.content_id,
+        playlistId,
         title: lockupView.metadata.title.text,
         thumbnail: lockupView.content_image.primary_thumbnail.image[0].url,
         channelName,
@@ -1180,6 +1189,7 @@ function parseLockupView(lockupView, channelId = undefined, channelName = undefi
     }
     default:
       console.warn(`Unknown lockup content type: ${lockupView.content_type}`, lockupView)
+      return null
   }
 }
 
