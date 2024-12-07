05858be9516ef2d27851cda518e7f8807df18858
Jason Henriquez <jasonhenriquez@protonmail.com>
2024-12-07 07:58:22 -0600

    Implement theatre mode setting migration

--- src/datastores/handlers/base.js
+++ src/datastores/handlers/base.js
@@ -25,6 +25,16 @@ class Settings {
       await this.upsert('externalPlayerCustomArgs', newValue)
     }
 
+    // In FreeTube 0.23.0, the "Enable Theatre Mode by Default" setting was incoporated as an option
+    // of the "Default Viewing Mode" setting. This is a one time migration to preserve users'
+    // Theater Mode preference through this change.
+    const defaultTheatreMode = await db.settings.findOneAsync({ _id: 'defaultTheatreMode' })
+
+    if (defaultTheatreMode?.value) {
+      await this.upsert('defaultViewingMode', 'theatre')
+      await this.upsert('defaultTheatreMode', false)
+    }
+
     return db.settings.findAsync({ _id: { $ne: 'bounds' } })
   }
 
