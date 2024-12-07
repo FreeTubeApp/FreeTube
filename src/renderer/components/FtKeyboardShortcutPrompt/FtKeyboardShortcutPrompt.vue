c6da141c2da34456f32b6337c9b63635bab906bc
Jason Henriquez <jasonhenriquez@protonmail.com>
2024-12-07 09:04:21 -0600

    Use usei18n instead of i18n.t

--- src/renderer/components/FtKeyboardShortcutPrompt/FtKeyboardShortcutPrompt.vue
+++ src/renderer/components/FtKeyboardShortcutPrompt/FtKeyboardShortcutPrompt.vue
@@ -60,8 +60,10 @@ import { KeyboardShortcuts } from '../../../constants'
 import { getLocalizedShortcut } from '../../helpers/utils'
 import FtPrompt from '../ft-prompt/ft-prompt.vue'
 import store from '../../store/index'
-import i18n from '../../i18n/index'
+import { useI18n } from '../../composables/use-i18n-polyfill'
 import FtIconButton from '../ft-icon-button/ft-icon-button.vue'
+
+const { t } = useI18n()
 
 const generalPlayerShortcuts = computed(() =>
   getLocalizedShortcutNamesAndValues(KeyboardShortcuts.VIDEO_PLAYER.GENERAL)
@@ -83,11 +85,11 @@ const primarySections = computed(() => [
   {
     secondarySections: [
       {
-        title: i18n.t('KeyboardShortcutPrompt.Sections.Video.Playback'),
+        title: t('KeyboardShortcutPrompt.Sections.Video.Playback'),
         shortcutDictionary: playbackPlayerShortcuts.value
       },
       {
-        title: i18n.t('KeyboardShortcutPrompt.Sections.Video.General'),
+        title: t('KeyboardShortcutPrompt.Sections.Video.General'),
         shortcutDictionary: generalPlayerShortcuts.value
       },
     ]
@@ -95,11 +97,11 @@ const primarySections = computed(() => [
   {
     secondarySections: [
       {
-        title: i18n.t('KeyboardShortcutPrompt.Sections.App.General'),
+        title: t('KeyboardShortcutPrompt.Sections.App.General'),
         shortcutDictionary: generalAppShortcuts.value
       },
       {
-        title: i18n.t('KeyboardShortcutPrompt.Sections.App.Situational'),
+        title: t('KeyboardShortcutPrompt.Sections.App.Situational'),
         shortcutDictionary: situationalAppShortcuts.value
       }
     ]
@@ -108,64 +110,64 @@ const primarySections = computed(() => [
 
 const isMac = process.platform === 'darwin'
 
-const title = computed(() => i18n.t('KeyboardShortcutPrompt.Keyboard Shortcuts'))
+const title = computed(() => t('KeyboardShortcutPrompt.Keyboard Shortcuts'))
 
-const closeButtonTitle = computed(() => i18n.t('Close'))
+const closeButtonTitle = computed(() => t('Close'))
 
 const localizedShortcutNameDictionary = computed(() => {
   return new Map([
-    ['SHOW_SHORTCUTS', i18n.t('KeyboardShortcutPrompt.Show Keyboard Shortcuts')],
-    ['HISTORY_BACKWARD', i18n.t('KeyboardShortcutPrompt.History Backward')],
-    ['HISTORY_FORWARD', i18n.t('KeyboardShortcutPrompt.History Forward')],
-    ['FULLSCREEN', i18n.t('KeyboardShortcutPrompt.Fullscreen')],
-    ['NAVIGATE_TO_SETTINGS', i18n.t('KeyboardShortcutPrompt.Navigate to Settings')],
+    ['SHOW_SHORTCUTS', t('KeyboardShortcutPrompt.Show Keyboard Shortcuts')],
+    ['HISTORY_BACKWARD', t('KeyboardShortcutPrompt.History Backward')],
+    ['HISTORY_FORWARD', t('KeyboardShortcutPrompt.History Forward')],
+    ['FULLSCREEN', t('KeyboardShortcutPrompt.Fullscreen')],
+    ['NAVIGATE_TO_SETTINGS', t('KeyboardShortcutPrompt.Navigate to Settings')],
     (
       isMac
-        ? ['NAVIGATE_TO_HISTORY_MAC', i18n.t('KeyboardShortcutPrompt.Navigate to History')]
-        : ['NAVIGATE_TO_HISTORY', i18n.t('KeyboardShortcutPrompt.Navigate to History')]
+        ? ['NAVIGATE_TO_HISTORY_MAC', t('KeyboardShortcutPrompt.Navigate to History')]
+        : ['NAVIGATE_TO_HISTORY', t('KeyboardShortcutPrompt.Navigate to History')]
     ),
-    ['NEW_WINDOW', i18n.t('KeyboardShortcutPrompt.New Window')],
-    ['MINIMIZE_WINDOW', i18n.t('KeyboardShortcutPrompt.Minimize Window')],
-    ['CLOSE_WINDOW', i18n.t('KeyboardShortcutPrompt.Close Window')],
+    ['NEW_WINDOW', t('KeyboardShortcutPrompt.New Window')],
+    ['MINIMIZE_WINDOW', t('KeyboardShortcutPrompt.Minimize Window')],
+    ['CLOSE_WINDOW', t('KeyboardShortcutPrompt.Close Window')],
     (isMac
-      ? ['QUIT_MAC', i18n.t('KeyboardShortcutPrompt.Quit')]
-      : ['QUIT', i18n.t('KeyboardShortcutPrompt.Quit')]
+      ? ['QUIT_MAC', t('KeyboardShortcutPrompt.Quit')]
+      : ['QUIT', t('KeyboardShortcutPrompt.Quit')]
     ),
-    ['RELOAD', i18n.t('KeyboardShortcutPrompt.Reload')],
-    ['FORCE_RELOAD', i18n.t('KeyboardShortcutPrompt.Force Reload')],
-    ['TOGGLE_DEVTOOLS', i18n.t('KeyboardShortcutPrompt.Toggle Developer Tools')],
-    ['RESET_ZOOM', i18n.t('KeyboardShortcutPrompt.Reset Zoom')],
-    ['ZOOM_IN', i18n.t('KeyboardShortcutPrompt.Zoom In')],
-    ['ZOOM_OUT', i18n.t('KeyboardShortcutPrompt.Zoom Out')],
-    ['FOCUS_SEARCH', i18n.t('KeyboardShortcutPrompt.Focus Search')],
-    ['SEARCH_IN_NEW_WINDOW', i18n.t('KeyboardShortcutPrompt.Search in New Window')],
+    ['RELOAD', t('KeyboardShortcutPrompt.Reload')],
+    ['FORCE_RELOAD', t('KeyboardShortcutPrompt.Force Reload')],
+    ['TOGGLE_DEVTOOLS', t('KeyboardShortcutPrompt.Toggle Developer Tools')],
+    ['RESET_ZOOM', t('KeyboardShortcutPrompt.Reset Zoom')],
+    ['ZOOM_IN', t('KeyboardShortcutPrompt.Zoom In')],
+    ['ZOOM_OUT', t('KeyboardShortcutPrompt.Zoom Out')],
+    ['FOCUS_SEARCH', t('KeyboardShortcutPrompt.Focus Search')],
+    ['SEARCH_IN_NEW_WINDOW', t('KeyboardShortcutPrompt.Search in New Window')],
 
-    ['REFRESH', i18n.t('KeyboardShortcutPrompt.Refresh')],
-    ['FOCUS_SECONDARY_SEARCH', i18n.t('KeyboardShortcutPrompt.Focus Secondary Search')],
+    ['REFRESH', t('KeyboardShortcutPrompt.Refresh')],
+    ['FOCUS_SECONDARY_SEARCH', t('KeyboardShortcutPrompt.Focus Secondary Search')],
 
-    ['CAPTIONS', i18n.t('KeyboardShortcutPrompt.Captions')],
-    ['THEATRE_MODE', i18n.t('KeyboardShortcutPrompt.Theatre Mode')],
-    ['FULLSCREEN', i18n.t('KeyboardShortcutPrompt.Fullscreen')],
-    ['FULLWINDOW', i18n.t('KeyboardShortcutPrompt.Full Window')],
-    ['PICTURE_IN_PICTURE', i18n.t('KeyboardShortcutPrompt.Picture in Picture')],
-    ['MUTE', i18n.t('KeyboardShortcutPrompt.Mute')],
-    ['VOLUME_UP', i18n.t('KeyboardShortcutPrompt.Volume Up')],
-    ['VOLUME_DOWN', i18n.t('KeyboardShortcutPrompt.Volume Down')],
-    ['TAKE_SCREENSHOT', i18n.t('KeyboardShortcutPrompt.Take Screenshot')],
-    ['STATS', i18n.t('KeyboardShortcutPrompt.Stats')],
+    ['CAPTIONS', t('KeyboardShortcutPrompt.Captions')],
+    ['THEATRE_MODE', t('KeyboardShortcutPrompt.Theatre Mode')],
+    ['FULLSCREEN', t('KeyboardShortcutPrompt.Fullscreen')],
+    ['FULLWINDOW', t('KeyboardShortcutPrompt.Full Window')],
+    ['PICTURE_IN_PICTURE', t('KeyboardShortcutPrompt.Picture in Picture')],
+    ['MUTE', t('KeyboardShortcutPrompt.Mute')],
+    ['VOLUME_UP', t('KeyboardShortcutPrompt.Volume Up')],
+    ['VOLUME_DOWN', t('KeyboardShortcutPrompt.Volume Down')],
+    ['TAKE_SCREENSHOT', t('KeyboardShortcutPrompt.Take Screenshot')],
+    ['STATS', t('KeyboardShortcutPrompt.Stats')],
 
-    ['PLAY', i18n.t('KeyboardShortcutPrompt.Play')],
-    ['LARGE_REWIND', i18n.t('KeyboardShortcutPrompt.Large Rewind')],
-    ['LARGE_FAST_FORWARD', i18n.t('KeyboardShortcutPrompt.Large Fast Forward')],
-    ['SMALL_REWIND', i18n.t('KeyboardShortcutPrompt.Small Rewind')],
-    ['SMALL_FAST_FORWARD', i18n.t('KeyboardShortcutPrompt.Small Fast Forward')],
-    ['DECREASE_VIDEO_SPEED', i18n.t('KeyboardShortcutPrompt.Decrease Video Speed')],
-    ['INCREASE_VIDEO_SPEED', i18n.t('KeyboardShortcutPrompt.Increase Video Speed')],
-    ['SKIP_N_TENTHS', i18n.t('KeyboardShortcutPrompt.Skip by Tenths')],
-    ['LAST_CHAPTER', i18n.t('KeyboardShortcutPrompt.Last Chapter')],
-    ['NEXT_CHAPTER', i18n.t('KeyboardShortcutPrompt.Next Chapter')],
-    ['LAST_FRAME', i18n.t('KeyboardShortcutPrompt.Last Frame')],
-    ['NEXT_FRAME', i18n.t('KeyboardShortcutPrompt.Next Frame')],
+    ['PLAY', t('KeyboardShortcutPrompt.Play')],
+    ['LARGE_REWIND', t('KeyboardShortcutPrompt.Large Rewind')],
+    ['LARGE_FAST_FORWARD', t('KeyboardShortcutPrompt.Large Fast Forward')],
+    ['SMALL_REWIND', t('KeyboardShortcutPrompt.Small Rewind')],
+    ['SMALL_FAST_FORWARD', t('KeyboardShortcutPrompt.Small Fast Forward')],
+    ['DECREASE_VIDEO_SPEED', t('KeyboardShortcutPrompt.Decrease Video Speed')],
+    ['INCREASE_VIDEO_SPEED', t('KeyboardShortcutPrompt.Increase Video Speed')],
+    ['SKIP_N_TENTHS', t('KeyboardShortcutPrompt.Skip by Tenths')],
+    ['LAST_CHAPTER', t('KeyboardShortcutPrompt.Last Chapter')],
+    ['NEXT_CHAPTER', t('KeyboardShortcutPrompt.Next Chapter')],
+    ['LAST_FRAME', t('KeyboardShortcutPrompt.Last Frame')],
+    ['NEXT_FRAME', t('KeyboardShortcutPrompt.Next Frame')],
   ])
 })
 
