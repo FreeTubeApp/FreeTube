0fcf1bec04ce80d6f39e425e32d10f94d342ce84
Jie Li <91730179+JL0000@users.noreply.github.com>
2024-12-19 23:03:28 +0000

    Added buttons to hide hidden channels/text (#6156)
    * added buttons to hide hidden content
    
    * renamed variables
    
    * capitalize text
    
    * changed showTags type to boolean
    
    * updated variable names
    
    * updated toggle name
    
    * changed toggle to checkboxes
    
    * add comma
    
    * removed redundant strings
    
    * made settings permanent
    
    * changed hide to show
    
    * show checkbox only where there are items and moved all the toggle logic to parent
    
    * updated checkbox colour and removed redundant html components
    
    * renamed variables
    
    * renamed variables
    
    * sanitised id

--- src/renderer/components/distraction-settings/distraction-settings.vue
+++ src/renderer/components/distraction-settings/distraction-settings.vue
@@ -246,10 +246,12 @@
         :validate-tag-name="validateChannelId"
         :find-tag-info="findChannelTagInfo"
         :are-channel-tags="true"
+        :show-tags="showAddedChannelsHidden"
         @invalid-name="handleInvalidChannel"
         @error-find-tag-info="handleChannelAPIError"
         @change="handleChannelsHidden"
         @already-exists="handleChannelsExists"
+        @toggle-show-tags="handleAddedChannelsHidden"
       />
     </ft-flex-box>
     <ft-flex-box>
@@ -257,10 +259,12 @@
         :label="$t('Settings.Distraction Free Settings.Hide Videos and Playlists Containing Text')"
         :tag-name-placeholder="$t('Settings.Distraction Free Settings.Hide Videos and Playlists Containing Text Placeholder')"
         :show-action-button="true"
+        :show-tags="showAddedForbiddenTitles"
         :tag-list="forbiddenTitles"
         :min-input-length="3"
         :tooltip="$t('Tooltips.Distraction Free Settings.Hide Videos and Playlists Containing Text')"
         @change="handleForbiddenTitles"
+        @toggle-show-tags="handleAddedForbiddenTitles"
       />
     </ft-flex-box>
   </ft-settings-section>
