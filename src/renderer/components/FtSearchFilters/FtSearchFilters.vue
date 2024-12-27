508e6167ae5644ae993757365d878aa81793bd33
absidue <48293849+absidue@users.noreply.github.com>
2024-12-27 21:57:21 +0100

    FtRadioButton and FtCheckboxList v-model and composition API

--- src/renderer/components/FtSearchFilters/FtSearchFilters.vue
+++ src/renderer/components/FtSearchFilters/FtSearchFilters.vue
@@ -13,49 +13,39 @@
       </h2>
       <FtFlexBox class="radioFlexBox">
         <FtRadioButton
-          ref="sortByRadio"
+          v-model="sortByValue"
           :title="$t('Search Filters.Sort By.Sort By')"
           :labels="sortByLabels"
           :values="SORT_BY_VALUES"
-          :initial-value-index="searchSortByStartIndex"
           class="searchRadio"
-          @change="updateSortBy"
         />
         <FtRadioButton
-          ref="timeRadio"
+          v-model="timeValue"
           :title="$t('Search Filters.Time.Time')"
           :labels="timeLabels"
           :values="TIME_VALUES"
-          :initial-value-index="searchTimeStartIndex"
           class="searchRadio"
-          @change="updateTime"
         />
         <FtRadioButton
-          ref="typeRadio"
+          v-model="typeValue"
           :title="$t('Search Filters.Type.Type')"
           :labels="typeLabels"
           :values="TYPE_VALUES"
-          :initial-value-index="searchTypeStartIndex"
           class="searchRadio"
-          @change="updateType"
         />
         <FtRadioButton
-          ref="durationRadio"
+          v-model="durationValue"
           :title="$t('Search Filters.Duration.Duration')"
           :labels="durationLabels"
           :values="DURATION_VALUES"
-          :initial-value-index="searchDurationStartIndex"
           class="searchRadio"
-          @change="updateDuration"
         />
         <FtCheckboxList
-          ref="featuresCheck"
+          v-model="featuresValue"
           :title="$t('Search Filters.Features.Features')"
           :labels="featureLabels"
           :values="FEATURE_VALUES"
-          :initial-values="searchDefaultFeatures"
           class="searchRadio"
-          @change="updateFeatures"
         />
       </FtFlexBox>
       <div class="searchFilterCloseButtonContainer">
@@ -71,14 +61,14 @@
 </template>
 
 <script setup>
-import { computed, ref } from 'vue'
+import { computed, ref, watch } from 'vue'
 import { useI18n } from '../../composables/use-i18n-polyfill'
 
 import FtFlexBox from '../ft-flex-box/ft-flex-box.vue'
-import FtRadioButton from '../ft-radio-button/ft-radio-button.vue'
+import FtRadioButton from '../FtRadioButton/FtRadioButton.vue'
 import FtPrompt from '../ft-prompt/ft-prompt.vue'
 import FtButton from '../ft-button/ft-button.vue'
-import FtCheckboxList from '../ft-checkbox-list/ft-checkbox-list.vue'
+import FtCheckboxList from '../FtCheckboxList/FtCheckboxList.vue'
 
 import store from '../../store/index'
 
@@ -181,31 +171,79 @@ const featureLabels = computed(() => [
   t('Search Filters.Features.VR180')
 ])
 
-const sortByRadio = ref(null)
-const timeRadio = ref(null)
-const typeRadio = ref(null)
-const durationRadio = ref(null)
-const featuresCheck = ref(null)
+const searchSettings = store.getters.getSearchSettings
 
-const searchFilterValueChanged = computed(() => {
-  if (sortByRadio.value == null || timeRadio.value == null || typeRadio.value == null || durationRadio.value == null || featuresCheck.value == null) {
-    return false
-  }
+/** @type {import('vue').Ref<'relevance' | 'rating' | 'upload_date' | 'view_count'>} */
+const sortByValue = ref(searchSettings.sortBy)
 
-  return sortByRadio.value.selectedValue !== SORT_BY_VALUES[0] ||
-    timeRadio.value.selectedValue !== TIME_VALUES[0] ||
-    typeRadio.value.selectedValue !== TYPE_VALUES[0] ||
-    durationRadio.value.selectedValue !== DURATION_VALUES[0] ||
-    featuresCheck.value.selectedValues.length > 0
+watch(sortByValue, (value) => {
+  store.commit('setSearchSortBy', value)
 })
 
-const searchSettings = computed(() => store.getters.getSearchSettings)
+/** @type {import('vue').Ref<'' | 'hour' | 'today' | 'week' | 'month' | 'year'>} */
+const timeValue = ref(searchSettings.time)
 
-const searchSortByStartIndex = SORT_BY_VALUES.indexOf(searchSettings.value.sortBy)
-const searchTimeStartIndex = TIME_VALUES.indexOf(searchSettings.value.time)
-const searchTypeStartIndex = TYPE_VALUES.indexOf(searchSettings.value.type)
-const searchDurationStartIndex = DURATION_VALUES.indexOf(searchSettings.value.duration)
-const searchDefaultFeatures = [...searchSettings.value.features]
+watch(timeValue, (value) => {
+  if (timeValue.value !== '' && !isVideoOrMovieOrAll(typeValue.value)) {
+    typeValue.value = 'all'
+  }
+
+  store.commit('setSearchTime', value)
+})
+
+/** @type {import('vue').Ref<'all' | 'video' | 'channel' | 'playlist' | 'movie'>} */
+const typeValue = ref(searchSettings.type)
+
+watch(typeValue, (value) => {
+  if (value === 'channel' || value === 'playlist') {
+    timeValue.value = ''
+    durationValue.value = ''
+    sortByValue.value = SORT_BY_VALUES[0]
+    if (featuresValue.value.length > 0) {
+      featuresValue.value = []
+    }
+  } else if (value === 'movie') {
+    if (featuresValue.value.length > 0) {
+      featuresValue.value = featuresValue.value.filter(e => !NOT_ALLOWED_FOR_MOVIES_FEATURES.includes(e))
+    }
+  }
+
+  store.commit('setSearchType', value)
+})
+
+/** @type {import('vue').Ref<'' | 'short' | 'medium' | 'long'>} */
+const durationValue = ref(searchSettings.duration)
+
+watch(durationValue, (value) => {
+  if (value !== '' && !isVideoOrMovieOrAll(typeValue.value)) {
+    typeValue.value = 'all'
+  }
+
+  store.commit('setSearchDuration', value)
+})
+
+/** @type {import('vue').Ref<('hd' | 'subtitles' | 'creative_commons' | '3d' | 'live' | '4k' | '360' | 'location' | 'hdr' | 'vr180')[]>} */
+const featuresValue = ref([...searchSettings.features])
+
+watch(featuresValue, (values) => {
+  if (values.length > 0 && (!isVideoOrMovieOrAll(typeValue.value) || NOT_ALLOWED_FOR_MOVIES_FEATURES.some(item => values.includes(item)))) {
+    typeValue.value = 'all'
+  }
+
+  store.commit('setSearchFeatures', [...values])
+}, { deep: true })
+
+const searchFilterValueChanged = computed(() => {
+  return sortByValue.value !== SORT_BY_VALUES[0] ||
+    timeValue.value !== TIME_VALUES[0] ||
+    typeValue.value !== TYPE_VALUES[0] ||
+    durationValue.value !== DURATION_VALUES[0] ||
+    featuresValue.value.length > 0
+})
+
+watch(searchFilterValueChanged, (value) => {
+  store.commit('setSearchFilterValueChanged', value)
+})
 
 function hideSearchFilters() {
   store.dispatch('hideSearchFilters')
@@ -215,79 +253,7 @@ function hideSearchFilters() {
  * @param {'all' | 'video' | 'channel' | 'playlist' | 'movie'} type
  */
 function isVideoOrMovieOrAll(type) {
-  return type === 'video' | type === 'movie' || type === 'all'
-}
-
-/**
- * @param {'relevance' | 'rating' | 'upload_date' | 'view_count'} value
- */
-function updateSortBy(value) {
-  store.commit('setSearchSortBy', value)
-  store.commit('setSearchFilterValueChanged', searchFilterValueChanged.value)
-}
-
-/**
- * @param {'' | 'hour' | 'today' | 'week' | 'month' | 'year'} value
- */
-function updateTime(value) {
-  if (!isVideoOrMovieOrAll(searchSettings.value.type)) {
-    typeRadio.value.updateSelectedValue('all')
-    store.commit('setSearchType', 'all')
-  }
-
-  store.commit('setSearchTime', value)
-  store.commit('setSearchFilterValueChanged', searchFilterValueChanged.value)
-}
-
-/**
- * @param {('hd' | 'subtitles' | 'creative_commons' | '3d' | 'live' | '4k' | '360' | 'location' | 'hdr' | 'vr180')[]} values
- */
-function updateFeatures(values) {
-  if (!isVideoOrMovieOrAll(searchSettings.value.type) || NOT_ALLOWED_FOR_MOVIES_FEATURES.some(item => values.includes(item))) {
-    typeRadio.value.updateSelectedValue('all')
-    store.commit('setSearchType', 'all')
-  }
-
-  store.commit('setSearchFeatures', values)
-  store.commit('setSearchFilterValueChanged', searchFilterValueChanged.value)
-}
-
-/**
- * @param {'all' | 'video' | 'channel' | 'playlist' | 'movie'} value
- */
-function updateType(value) {
-  if (value === 'channel' || value === 'playlist') {
-    timeRadio.value.updateSelectedValue('')
-    durationRadio.value.updateSelectedValue('')
-    sortByRadio.value.updateSelectedValue(SORT_BY_VALUES[0])
-    featuresCheck.value.setSelectedValues([])
-
-    store.commit('setSearchTime', '')
-    store.commit('setSearchDuration', '')
-    store.commit('setSearchFeatures', [])
-    store.commit('setSearchSortBy', SORT_BY_VALUES[0])
-  } else if (value === 'movie') {
-    const filteredFeatures = searchSettings.value.features.filter(e => !NOT_ALLOWED_FOR_MOVIES_FEATURES.includes(e))
-
-    featuresCheck.value.setSelectedValues([...filteredFeatures])
-    store.commit('setSearchFeatures', filteredFeatures)
-  }
-
-  store.commit('setSearchType', value)
-  store.commit('setSearchFilterValueChanged', searchFilterValueChanged.value)
-}
-
-/**
- * @param {'' | 'short' | 'medium' | 'long'} value
- */
-function updateDuration(value) {
-  if (value !== '' && !isVideoOrMovieOrAll(searchSettings.value.type)) {
-    typeRadio.value.updateSelectedValue('all')
-    updateType('all')
-  }
-
-  store.commit('setSearchDuration', value)
-  store.commit('setSearchFilterValueChanged', searchFilterValueChanged.value)
+  return type === 'video' || type === 'movie' || type === 'all'
 }
 </script>
 
