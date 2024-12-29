d18061242593d74bbe02a4c979ac4d5700f331b7
GLEBR1K <28905234+GLEBR1K@users.noreply.github.com>
2024-12-29 00:34:48 +0000

    [Feature] Playlist: Sort videos by published date (#6280)
    * include `published` info for playlisted videos
    
    * playlist: sort videos by published date
    
    * playlist: fix published date formatting for upcoming videos
    
    * playlist: add `premiereDate` to the metadata

--- src/renderer/components/ft-list-video/ft-list-video.js
+++ src/renderer/components/ft-list-video/ft-list-video.js
@@ -775,6 +775,9 @@ export default defineComponent({
         description: this.description,
         viewCount: this.viewCount,
         lengthSeconds: this.data.lengthSeconds,
+        published: this.published,
+        premiereDate: this.data.premiereDate,
+        premiereTimestamp: this.data.premiereTimestamp,
       }
 
       this.showAddToPlaylistPromptForManyVideos({ videos: [videoData] })
@@ -824,6 +827,9 @@ export default defineComponent({
         author: this.channelName,
         authorId: this.channelId,
         lengthSeconds: this.data.lengthSeconds,
+        published: this.published,
+        premiereDate: this.data.premiereDate,
+        premiereTimestamp: this.data.premiereTimestamp,
       }
 
       this.addVideo({
--- src/renderer/components/watch-video-info/watch-video-info.js
+++ src/renderer/components/watch-video-info/watch-video-info.js
@@ -39,6 +39,10 @@ export default defineComponent({
       type: Number,
       required: true
     },
+    premiereDate: {
+      type: Date,
+      default: undefined
+    },
     viewCount: {
       type: Number,
       required: true
@@ -348,6 +352,8 @@ export default defineComponent({
         description: this.description,
         viewCount: this.viewCount,
         lengthSeconds: this.lengthSeconds,
+        published: this.published,
+        premiereDate: this.premiereDate,
       }
 
       this.showAddToPlaylistPromptForManyVideos({ videos: [videoData] })
@@ -372,6 +378,8 @@ export default defineComponent({
         author: this.channelName,
         authorId: this.channelId,
         lengthSeconds: this.lengthSeconds,
+        published: this.published,
+        premiereDate: this.premiereDate,
       }
 
       this.addVideo({
