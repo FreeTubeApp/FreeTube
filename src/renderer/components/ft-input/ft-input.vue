ea73ad13e97fbe5f17e4132c0662b31c624ac368
Jason Henriquez <jasonhenriquez@protonmail.com>
2024-12-28 20:04:13 -0600

    Fix text overflow issue with long search history entries

--- src/renderer/components/ft-input/ft-input.vue
+++ src/renderer/components/ft-input/ft-input.vue
@@ -84,12 +84,14 @@
           @mouseenter="searchState.selectedOption = index"
           @mouseleave="searchState.selectedOption = -1; removeButtonSelectedIndex = -1"
         >
-          <font-awesome-icon
-            v-if="dataListProperties[index]?.iconName"
-            :icon="['fas', dataListProperties[index].iconName]"
-            class="searchResultIcon"
-          />
-          <span>{{ entry }}</span>
+          <div>
+            <font-awesome-icon
+              v-if="dataListProperties[index]?.iconName"
+              :icon="['fas', dataListProperties[index].iconName]"
+              class="searchResultIcon"
+            />
+            <span>{{ entry }}</span>
+          </div>
           <a
             v-if="dataListProperties[index]?.isRemoveable"
             class="removeButton"
