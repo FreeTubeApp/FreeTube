b764fd3b26bf822d3da90808eb55d9e7cf653c3d
absidue <48293849+absidue@users.noreply.github.com>
2024-12-31 01:09:52 +0100

    Remove buggy VA-API on Linux flag (#6489)

--- src/main/index.js
+++ src/main/index.js
@@ -209,14 +209,6 @@ function runApp() {
   let mainWindow
   let startupUrl
 
-  if (process.platform === 'linux') {
-    // Enable hardware acceleration via VA-API with OpenGL if no other feature flags are found
-    // https://chromium.googlesource.com/chromium/src/+/refs/heads/main/docs/gpu/vaapi.md
-    if (!app.commandLine.hasSwitch('enable-features')) {
-      app.commandLine.appendSwitch('enable-features', 'VaapiVideoDecodeLinuxGL')
-    }
-  }
-
   const userDataPath = app.getPath('userData')
 
   // command line switches need to be added before the app ready event first
