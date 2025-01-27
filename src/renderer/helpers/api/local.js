9a7f2793784c9a152a27ce82db6208d8ecdd79cc
ChunkyProgrammer <78101139+ChunkyProgrammer@users.noreply.github.com>
2025-01-26 21:32:40 -0500

    use array index instead of `first()`
    Co-authored-by: absidue <48293849+absidue@users.noreply.github.com>

--- src/renderer/helpers/api/local.js
+++ src/renderer/helpers/api/local.js
@@ -1716,7 +1716,7 @@ export async function getLocalCommunityPost(postId, channelId) {
   }
 
   const postPage = await innertube.getPost(postId, channelId)
-  return parseLocalCommunityPost(postPage.posts.first())
+  return parseLocalCommunityPost(postPage.posts[0])
 }
 
 /**
