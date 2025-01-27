cd69e1bd511c62fc765aad354e1242f6d17a2818
absidue <48293849+absidue@users.noreply.github.com>
2025-01-27 20:50:33 +0100

    Migrate ProfileSettings, FtProfileBubble and FtProfileEdit to the composition API (#6639)

--- src/renderer/views/ProfileSettings/ProfileSettings.vue
+++ src/renderer/views/ProfileSettings/ProfileSettings.vue
@@ -1,43 +1,44 @@
 <template>
   <div>
-    <ft-card class="card">
+    <FtCard class="card">
       <h2>{{ $t("Profile.Profile Manager") }}</h2>
-      <ft-flex-box
+      <FtFlexBox
         class="profileList"
       >
-        <ft-profile-bubble
-          v-for="(profile, index) in profileList"
-          :key="index"
-          :profile-id="profile._id"
+        <FtProfileBubble
+          v-for="profile in profileList"
+          :key="profile._id"
+          :is-main-profile="profile._id === MAIN_PROFILE_ID"
           :profile-name="profile.name"
           :background-color="profile.bgColor"
           :text-color="profile.textColor"
           :class="{ openedProfile: openSettingsProfile?._id === profile._id }"
           @click="openSettingsForProfileWithId(profile._id)"
         />
-      </ft-flex-box>
-      <ft-flex-box>
-        <ft-button
-          v-if="!isNewProfileOpen"
+      </FtFlexBox>
+      <FtFlexBox
+        v-if="!isNewProfileOpen"
+      >
+        <FtButton
           :label="$t('Profile.Create New Profile')"
           @click="openSettingsForNewProfile"
         />
-      </ft-flex-box>
-    </ft-card>
+      </FtFlexBox>
+    </FtCard>
     <div
       v-if="openSettingsProfile"
       :key="openSettingsProfileId"
     >
-      <ft-profile-channel-list
+      <FtProfileChannelList
         v-if="!isNewProfileOpen"
         :profile="openSettingsProfile"
         :is-main-profile="isMainProfile"
       />
-      <ft-profile-filter-channels-list
+      <FtProfileFilterChannelsList
         v-if="!isNewProfileOpen && !isMainProfile"
         :profile="openSettingsProfile"
       />
-      <ft-profile-edit
+      <FtProfileEdit
         :profile="openSettingsProfile"
         :is-new="isNewProfileOpen"
         :is-main-profile="isMainProfile"
@@ -48,5 +49,102 @@
   </div>
 </template>
 
-<script src="./ProfileSettings.js" />
+<script setup>
+import { computed, ref, shallowRef, watch } from 'vue'
+
+import FtCard from '../../components/ft-card/ft-card.vue'
+import FtFlexBox from '../../components/ft-flex-box/ft-flex-box.vue'
+import FtProfileBubble from '../../components/FtProfileBubble/FtProfileBubble.vue'
+import FtButton from '../../components/ft-button/ft-button.vue'
+import FtProfileEdit from '../../components/FtProfileEdit/FtProfileEdit.vue'
+import FtProfileChannelList from '../../components/ft-profile-channel-list/ft-profile-channel-list.vue'
+import FtProfileFilterChannelsList from '../../components/ft-profile-filter-channels-list/ft-profile-filter-channels-list.vue'
+
+import store from '../../store/index'
+
+import { calculateColorLuminance, getRandomColor } from '../../helpers/colors'
+import { MAIN_PROFILE_ID } from '../../../constants'
+
+/**
+ * @typedef {object} Profile
+ * @property {string} _id
+ * @property {string} name
+ * @property {string} bgColor
+ * @property {string} textColor
+ * @property {object[]} subscriptions
+ * @property {string} subscriptions[].id
+ * @property {string|undefined} subscriptions[].name
+ * @property {string|undefined} subscriptions[].thumbnail
+ */
+
+const isNewProfileOpen = ref(false)
+
+/** @type {import('vue').Ref<string>} */
+const openSettingsProfileId = ref('')
+
+/** @type {import('vue').ShallowRef<Profile|null>} */
+const openSettingsProfile = shallowRef(null)
+
+/** @type {import('vue').ComputedRef<Profile[]>} */
+const profileList = computed(() => {
+  return store.getters.getProfileList
+})
+
+watch(profileList, () => {
+  openSettingsProfile.value = getProfileById(openSettingsProfileId.value)
+}, { deep: true })
+
+const isMainProfile = computed(() => {
+  return MAIN_PROFILE_ID === openSettingsProfileId.value
+})
+
+function openSettingsForNewProfile() {
+  isNewProfileOpen.value = true
+
+  openSettingsProfile.value = {
+    name: '',
+    bgColor: getRandomColor().value,
+    textColor: calculateColorLuminance(getRandomColor().value),
+    subscriptions: []
+  }
+
+  openSettingsProfileId.value = ''
+}
+
+/**
+ * @param {string} profileId
+ */
+function openSettingsForProfileWithId(profileId) {
+  if (profileId === openSettingsProfileId.value) {
+    return
+  }
+
+  isNewProfileOpen.value = false
+  openSettingsProfileId.value = profileId
+  openSettingsProfile.value = getProfileById(profileId)
+}
+
+/**
+ * @param {string | null} profileId
+ */
+function getProfileById(profileId) {
+  if (!profileId) {
+    return null
+  }
+
+  return store.getters.profileById(profileId)
+}
+
+function handleNewProfileCreated() {
+  isNewProfileOpen.value = false
+  openSettingsProfile.value = null
+  openSettingsProfileId.value = ''
+}
+
+function handleProfileDeleted() {
+  openSettingsProfile.value = null
+  openSettingsProfileId.value = ''
+}
+</script>
+
 <style scoped src="./ProfileSettings.css" />
