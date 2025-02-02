c436c5e8ecafaf8ee9bce138bc297a14d9c8a478
Emma <MarmadileManteater@proton.me>
2025-02-02 14:31:57 -0500

    Migrate ft-slider to the composition api (#6677)
    * Migrate ft-slider to the composition api
    
    * Fix web webpack config
    
    * Use `useId`
    
    * Use `$t`
    
    * fix discrepancy

---
+++ src/renderer/components/FtSlider/FtSlider.vue
@@ -0,0 +1,75 @@
+<template>
+  <label
+    class="pure-material-slider"
+    :for="id"
+  >
+    <input
+      :id="id"
+      v-model.number="currentValue"
+      :disabled="disabled"
+      type="range"
+      :min="minValue"
+      :max="maxValue"
+      :step="step"
+      @change="change"
+    >
+    <span>
+      {{ $t('Display Label', {label: label, value: displayLabel}) }}
+    </span>
+  </label>
+</template>
+
+<script setup>
+import { computed, ref } from 'vue'
+import { useId } from '../../composables/use-id-polyfill.js'
+
+const props = defineProps({
+  label: {
+    type: String,
+    required: true
+  },
+  defaultValue: {
+    type: Number,
+    required: true
+  },
+  minValue: {
+    type: Number,
+    required: true
+  },
+  maxValue: {
+    type: Number,
+    required: true
+  },
+  step: {
+    type: Number,
+    required: true
+  },
+  valueExtension: {
+    type: String,
+    default: null
+  },
+  disabled: {
+    type: Boolean,
+    default: false
+  }
+})
+
+const emit = defineEmits(['change'])
+
+const id = useId()
+const currentValue = ref(props.defaultValue)
+
+const displayLabel = computed(() => {
+  if (props.valueExtension === null) {
+    return currentValue.value
+  } else {
+    return `${currentValue.value}${props.valueExtension}`
+  }
+})
+
+function change() {
+  emit('change', currentValue.value)
+}
+
+</script>
+<style scoped src="./FtSlider.css" />
