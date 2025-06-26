d0345ec9bc252c9ca0c86b0fc6e8ed810bb9b92e
ChunkyProgrammer <78101139+ChunkyProgrammer@users.noreply.github.com>
2025-06-25 22:20:27 -0400

    use newer params as well (old params still worked but no idea how long they will work for?)

--- src/renderer/helpers/api/local.js
+++ src/renderer/helpers/api/local.js
@@ -517,7 +517,7 @@ export async function getLocalChannelCommunity(id) {
   try {
     const response = await innertube.actions.execute('/browse', {
       browseId: id,
-      params: 'Egljb21tdW5pdHnyBgQKAkoA'
+      params: 'EgVwb3N0c_IGBAoCSgA%3D'
       // protobuf for the community tab (this is the one that YouTube uses,
       // it has some empty fields in the protobuf but it doesn't work if you remove them)
     })
