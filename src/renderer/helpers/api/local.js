8de6fbc63de9e5f10e2dfe40899cf5a2e5e058a0
ChunkyProgrammer <78101139+ChunkyProgrammer@users.noreply.github.com>
2025-06-27 13:19:54 +0000

    Fix getting community posts on subscription page (#7603)
    * Fix getting community posts on subscription page
    
    * use newer params as well (old params still worked but no idea how long they will work for?)

--- src/renderer/helpers/api/local.js
+++ src/renderer/helpers/api/local.js
@@ -518,7 +518,7 @@ export async function getLocalChannelCommunity(id) {
   try {
     const response = await innertube.actions.execute('/browse', {
       browseId: id,
-      params: 'Egljb21tdW5pdHnyBgQKAkoA'
+      params: 'EgVwb3N0c_IGBAoCSgA%3D'
       // protobuf for the community tab (this is the one that YouTube uses,
       // it has some empty fields in the protobuf but it doesn't work if you remove them)
     })
@@ -527,7 +527,7 @@ export async function getLocalChannelCommunity(id) {
 
     // if the channel doesn't have a community tab, YouTube returns the home tab instead
     // so we need to check that we got the right tab
-    if (communityTab.current_tab?.endpoint.metadata.url?.endsWith('/community')) {
+    if (communityTab.current_tab?.endpoint.metadata.url?.endsWith('/posts')) {
       return parseLocalCommunityPosts(communityTab.posts)
     } else {
       return []
