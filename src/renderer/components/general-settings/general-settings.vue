310593645a2ccff95b938806bb5385784db47a94
efb4f5ff-1298-471a-8973-3d47447115dc <73130443+efb4f5ff-1298-471a-8973-3d47447115dc@users.noreply.github.com>
2025-08-11 17:30:14 +0200

    Revert "set checkForBlogPosts to false"
    This reverts commit ae9bda3c4cdbaa4de040535c02dad4a5e97b348f.

--- src/renderer/components/general-settings/general-settings.vue
+++ src/renderer/components/general-settings/general-settings.vue
@@ -29,7 +29,7 @@
         <ft-toggle-switch
           :label="$t('Settings.General Settings.Check for Latest Blog Posts')"
           :default-value="checkForBlogPosts"
-          :compact="false"
+          :compact="true"
           @change="updateCheckForBlogPosts"
         />
         <ft-toggle-switch
