5d29887c4e0a5ce720136a01a442db6ed0a96976
absidue <48293849+absidue@users.noreply.github.com>
2025-11-02 15:03:56 +0100

    Remove broken restart window keyboard shortcut

--- src/main/index.js
+++ src/main/index.js
@@ -2197,11 +2197,6 @@ function runApp() {
       {
         label: 'View',
         submenu: [
-          { role: 'reload' },
-          {
-            role: 'forcereload',
-            accelerator: 'CmdOrCtrl+Shift+R'
-          },
           { role: 'toggledevtools' },
           { role: 'toggledevtools', accelerator: 'f12', visible: false },
           {
