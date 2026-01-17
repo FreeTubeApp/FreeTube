b06c902ea4b491cff3db11bc38c083fcf21b5d29
Lukas <38840142+lukasstorck@users.noreply.github.com>
2026-01-17 13:34:00 +0100

    fix: video player playback rate (#8407)
    * fix: load video player with correct value for default playback rate
    
    * fix: set playback rate and default playback rate after initializing the video player (calling .attach())
    
    * Remove unnecessary comment
    
    ---------
    
    Co-authored-by: absidue <48293849+absidue@users.noreply.github.com>

--- src/renderer/components/ft-shaka-video-player/ft-shaka-video-player.js
+++ src/renderer/components/ft-shaka-video-player/ft-shaka-video-player.js
@@ -2579,9 +2579,6 @@ export default defineComponent({
         videoElement.muted = (muted === 'true')
       }
 
-      videoElement.playbackRate = props.currentPlaybackRate
-      videoElement.defaultPlaybackRate = props.currentPlaybackRate
-
       const localPlayer = new shaka.Player()
 
       ui = new shaka.ui.Overlay(
@@ -2594,6 +2591,9 @@ export default defineComponent({
       // This has to be called after creating the UI, so that the player uses the UI's UITextDisplayer
       // otherwise it uses the browsers native captions which get displayed underneath the UI controls
       await localPlayer.attach(videoElement)
+
+      videoElement.playbackRate = props.currentPlaybackRate
+      videoElement.defaultPlaybackRate = defaultPlaybackRate.value
 
       // check if the component is already getting destroyed
       // which is possible because this function runs asynchronously
