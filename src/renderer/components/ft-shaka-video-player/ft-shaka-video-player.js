a229e6e36024d52d7b906e16aeccbb4963f5e8c2
Pillowytuba <progamerbrostx@gmail.com>
2026-02-24 20:40:25 -0600

    Fix dynamically creating a reactive object

--- src/renderer/components/ft-shaka-video-player/ft-shaka-video-player.js
+++ src/renderer/components/ft-shaka-video-player/ft-shaka-video-player.js
@@ -582,7 +582,7 @@ export default defineComponent({
             }, 1000)
             existingSkip.timeoutId = setTimeout(removeSegment, 4000)
           } else {
-            const segmentObj = reactive({
+            const segmentObj = {
               uuid,
               translatedCategory: translateSponsorBlockCategory(category),
               startTime,
@@ -591,14 +591,15 @@ export default defineComponent({
               timeLeft: 4,
               intervalId: null,
               timeoutId: null
-            })
-
-            segmentObj.intervalId = setInterval(() => {
-              segmentObj.timeLeft--
-            }, 1000)
-            segmentObj.timeoutId = setTimeout(removeSegment, 4000)
+            }
 
             skippedSponsorBlockSegments.value.push(segmentObj)
+            const reactiveSegmentObj = skippedSponsorBlockSegments.value.find(skipped => skipped.uuid === uuid)
+
+            reactiveSegmentObj.intervalId = setInterval(() => {
+              reactiveSegmentObj.timeLeft--
+            }, 1000)
+            reactiveSegmentObj.timeoutId = setTimeout(removeSegment, 4000)
           }
         })
       }
