ddaaa66e3240b35716716cf79da1e13380cc82d0
Cameron Radmore <radmorecameron@gmail.com>
2026-04-26 13:30:52 -0400

    fix for pnpm

--- playwright.config.mjs
+++ playwright.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('@playwright/test').PlaywrightTestConfig} */
 export default {
   testDir: './tests/playwright',
-  testMatch: '*.spec.mjs',
+  testMatch: '**/*.spec.mjs',
   workers: 1,
   fullyParallel: false
 }
