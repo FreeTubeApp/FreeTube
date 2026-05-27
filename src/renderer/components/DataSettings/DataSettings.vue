53a59b89acc41cf62e02323f0e8d50554307ba6b
David <david.cec@proton.me>
2026-05-28 00:32:25 +0200

    Make 'description' field optional when importing FreeTube wtahc history

--- src/renderer/components/DataSettings/DataSettings.vue
+++ src/renderer/components/DataSettings/DataSettings.vue
@@ -818,7 +818,6 @@ async function importFreeTubeWatchHistory(textDecode) {
   const requiredKeys = [
     'author',
     'authorId',
-    'description',
     'isLive',
     'lengthSeconds',
     'published',
@@ -836,6 +835,7 @@ async function importFreeTubeWatchHistory(textDecode) {
     'lastViewedPlaylistItemId',
     'lastViewedPlaylistType',
     'viewCount',
+    'description',
   ]
 
   const ignoredKeys = [
@@ -868,6 +868,9 @@ async function importFreeTubeWatchHistory(textDecode) {
       showToast(t('Settings.Data Settings.History object has insufficient data, skipping item'))
       console.error('Missing Keys: ', missingKeys, historyData)
     } else {
+      // FreeTube history export does not have this data if the video was marked as watched manually, setting default value
+      historyObject.description = ''
+
       historyItems.set(historyObject.videoId, historyObject)
     }
   })
