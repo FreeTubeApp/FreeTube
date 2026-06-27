7758ba55f201d222c2ffab1333f619cdce85621a
ozrendev <ozrendev@protonmail.com>
2026-06-27 17:43:54 +0800

    change max value when toggled

--- src/renderer/components/SubscriptionSettings/SubscriptionSettings.vue
+++ src/renderer/components/SubscriptionSettings/SubscriptionSettings.vue
@@ -78,6 +78,8 @@ function updateUseRssFeeds(value) {
   const max = value ? 15 : 30
   if (onlyShowLatestFromChannelNumber.value > max) {
     updateOnlyShowLatestFromChannelNumber(max)
+  } else if (!value && onlyShowLatestFromChannelNumber.value === 15) {
+    updateOnlyShowLatestFromChannelNumber(max)
   }
 }
 
