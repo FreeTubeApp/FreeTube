8879a78af422523960773dae3ea3f43e395a7c48
David <david.cec@proton.me>
2026-07-15 14:03:04 +0200

    Code clean-up

--- src/renderer/helpers/api/local.js
+++ src/renderer/helpers/api/local.js
@@ -1915,7 +1915,6 @@ function parseLockupView(lockupView, channelId = undefined, channelName = undefi
       let collaboratorIds = []
       if (maybeCollaborators) {
         collaboratorIds = maybeCollaborators.map(item => item.renderer_context?.command_context?.on_tap?.payload?.browseId)
-          .filter(collabChannelId => collabChannelId)
       }
 
       return {
