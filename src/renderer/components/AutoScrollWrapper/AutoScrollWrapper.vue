73de969d0428350c44ce17618d194ff148d8605e
PikachuEXE <git@pikachuexe.net>
2026-08-09 08:12:48 +0800

    * Make drag scroll speed 3x

--- src/renderer/components/AutoScrollWrapper/AutoScrollWrapper.vue
+++ src/renderer/components/AutoScrollWrapper/AutoScrollWrapper.vue
@@ -66,7 +66,7 @@ function clearScrollDirection() {
  * @param {'up' | 'down'} direction
  */
 function scroll(direction) {
-  const SPEED = 6
+  const SPEED = 18
 
   if (direction === 'up') {
     globalThis.scrollBy(0, -SPEED)
