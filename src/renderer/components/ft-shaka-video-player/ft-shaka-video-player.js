47e4bcec227b792957f19218f5c494306a64b71a
PikachuEXE <git@pikachuexe.net>
2026-08-22 00:09:15 +0800

    ! Fix legacy as default format but unavailable won't fallback to other formats (#9644)

--- src/renderer/components/ft-shaka-video-player/ft-shaka-video-player.js
+++ src/renderer/components/ft-shaka-video-player/ft-shaka-video-player.js
@@ -1518,6 +1518,7 @@ export default defineComponent({
 
       /** @type {object[]} */
       const legacyFormats = props.legacyFormats
+      if (legacyFormats.length === 0) { return }
 
       const isPortrait = legacyFormats[0].height > legacyFormats[0].width
 
@@ -2830,7 +2831,7 @@ export default defineComponent({
             forceAspectRatio.value = firstVariant.width / firstVariant.height < 1.5
           }
         })
-      } else {
+      } else if (props.legacyFormats.length > 0) {
         // force the player aspect ratio to 16:9 to avoid overflowing the layout, when the video is too tall
 
         const firstFormat = props.legacyFormats[0]
