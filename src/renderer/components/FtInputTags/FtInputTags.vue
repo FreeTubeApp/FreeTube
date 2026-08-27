70d4df6f6fe8a88e36849c1077b0e45ec1b72ed5
Cameron Radmore <radmorecameron@gmail.com>
2026-08-26 20:37:35 -0400

    Buttons: add missing labels

--- src/renderer/components/FtInputTags/FtInputTags.vue
+++ src/renderer/components/FtInputTags/FtInputTags.vue
@@ -18,6 +18,7 @@
       :tooltip="tooltip"
       :show-action-button="true"
       :select-on-focus="true"
+      :action-button-label="t('Settings.Distraction Free Settings.Add')"
       :force-action-button-icon-name="['fas', 'arrow-right']"
       @click="updateTags"
     />
@@ -69,6 +70,8 @@
           <button
             v-if="!disabled"
             class="removeTagButton"
+            :title="t('Settings.Distraction Free Settings.Remove')"
+            :aria-label="t('Settings.Distraction Free Settings.Remove')"
             @click="removeTag(tag)"
           >
             <FontAwesomeIcon
