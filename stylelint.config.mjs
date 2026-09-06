a7d326a446320a80fefc9efdb9b22824ebaaf5ae
Gnu-Gorets <164275518+Gnu-Gorets@users.noreply.github.com>
2026-09-06 15:12:31 +0200

    chore: upgrade pnpm and dependencies

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -9,6 +9,7 @@ export default {
       files: '**/*.scss',
       customSyntax: 'postcss-scss',
       rules: {
+        'at-rule-prelude-no-invalid': null,
         'max-nesting-depth': null,
         'selector-max-compound-selectors': null
       }
