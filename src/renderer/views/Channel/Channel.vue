63e0433e0b798ca02f4d17a42d68af4b8039f778
David <david.cec@proton.me>
2026-09-14 00:30:38 +0200

    Remove unused text

--- src/renderer/views/Channel/Channel.vue
+++ src/renderer/views/Channel/Channel.vue
@@ -109,13 +109,10 @@
           aria-labelledby="videosTab"
         />
         <FtFlexBox
-          v-if="currentTab === 'videos' && latestVideos.length === 0"
+          v-if="currentTab === 'videos' && latestVideos.length === 0 && showFetchMoreButton"
         >
           <p class="message">
-            {{ showFetchMoreButton
-              ? $t("Channel.Videos.This channel has videos but none could be displayed, try fetching more")
-              : $t("Channel.Videos.This channel does not currently have any videos")
-            }}
+            {{ $t("Channel.Videos.This channel has videos but none could be displayed, try fetching more") }}
           </p>
         </FtFlexBox>
         <FtElementList
