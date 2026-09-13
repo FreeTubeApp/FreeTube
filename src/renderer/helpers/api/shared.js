b82e3a8f5d179adea25368596ef5480681c48aa8
Cameron Radmore <radmorecameron@gmail.com>
2026-09-12 20:40:51 -0400

    Use ESM import

--- src/renderer/helpers/api/shared.js
+++ src/renderer/helpers/api/shared.js
@@ -1,9 +1,9 @@
+import { Utils } from 'youtubei.js'
+import { ClipParams } from '../../../../node_modules/youtubei.js/dist/protos/generated/misc/params'
+
 // code shared between invidious and local api (like parsing or types)
 export function parseVideoClipsParams(videoId, params) {
   if (process.env.SUPPORTS_LOCAL_API) {
-    const { Utils } = require('youtubei.js')
-    const { ClipParams } = require('../../../../node_modules/youtubei.js/dist/protos/generated/misc/params')
-
     const parsedParams = ClipParams.decode(Utils.base64ToU8(decodeURIComponent(params)))
 
     return {
