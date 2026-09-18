efcd3ca485b18df9b3de4b1e297b8ff17eaa27f2
efb4f5ff-1298-471a-8973-3d47447115dc <73130443+efb4f5ff-1298-471a-8973-3d47447115dc@users.noreply.github.com>
2026-09-18 23:27:26 +0200

    Use `list` icon instead of `bookmark` to represent Playlists (#9797)
    * use list icon instead of bookmark for playlist
    
    * change userplaylist header icon

--- src/renderer/components/SideNav/SideNav.vue
+++ src/renderer/components/SideNav/SideNav.vue
@@ -105,7 +105,7 @@
           class="thumbnailContainer"
         >
           <FontAwesomeIcon
-            :icon="['fas', 'bookmark']"
+            :icon="['fas', 'list']"
             class="navIcon"
             :class="applyNavIconExpand"
           />
