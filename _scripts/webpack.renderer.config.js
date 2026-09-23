f3199e5268f7d973084154bf9ac5fd8af8c2e664
absidue <48293849+absidue@users.noreply.github.com>
2026-09-23 03:36:49 +0200

    Remove babel build dependency (#9829)

--- _scripts/webpack.renderer.config.js
+++ _scripts/webpack.renderer.config.js
@@ -47,11 +47,6 @@ const config = {
   module: {
     rules: [
       {
-        test: /\.js$/,
-        use: 'babel-loader',
-        exclude: /node_modules/,
-      },
-      {
         test: /\.vue$/,
         loader: 'vue-loader',
         options: {
--- _scripts/webpack.web.config.js
+++ _scripts/webpack.web.config.js
@@ -37,11 +37,6 @@ const config = {
   module: {
     rules: [
       {
-        test: /\.js$/,
-        use: 'babel-loader',
-        exclude: /node_modules/,
-      },
-      {
         test: /\.vue$/,
         loader: 'vue-loader',
         options: {
